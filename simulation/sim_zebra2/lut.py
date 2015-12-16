95b66bdd3cef701be956f876b698cb36999b06c1
Chris Turner <chris.j.turner@diamond.ac.uk>
2015-12-16 11:54:41 +0000

    adds logic for the LUT simulation

--- simulation/sim_zebra2/lut.py
+++ simulation/sim_zebra2/lut.py
@@ -1,11 +1,24 @@
 from .block import Block
 from .event import Event
 
+
 class Lut(Block):
 
-    def do_pulse(self, next_event, event):
-        pass
+    def __init__(self, num):
+        super(Lut, self).__init__(num)
+        self.inputs = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0}
 
+    def do_lookup(self, next_event, event):
+        """We've received a bit event on an INPUT channel, set the local,
+        copies of the inputs and set the output from address value the inputs make."""
+        input_map = {self.INPA:'A', self.INPB:'B', self.INPC:'C', self.INPD:'D', self.INPE:'E'}
+        for name, val in event.bit.items():
+            self.inputs[input_map[name]] = val
+        x = self.get_address(self.inputs)
+        next_event.bit[self.VAL] = int('{0:032b}'.format(self.FUNC)[x])
+
+    def get_address(self, input):
+        return (input['A'] << 4) + (input['B'] << 3) + (input['C'] << 2) + (input['D'] << 1) + input['E']
 
     def on_event(self, event):
         """Handle register, bit and pos changes at a particular timestamps,
@@ -16,7 +29,7 @@ class Lut(Block):
             for name, value in event.reg.items():
                 setattr(self, name, value)
         # if we got an input, then process it
-        if any(x in event.bit for x in [self.INPA, self.INPB, self.INPC, self.INPD, self.INPE ]):
-            self.do_pulse(next_event, event)
+        if any(x in event.bit for x in [self.INPA, self.INPB, self.INPC, self.INPD, self.INPE]):
+            self.do_lookup(next_event, event)
         # return any changes and next ts
         return next_event
