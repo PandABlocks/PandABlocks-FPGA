771441dbaa55a927de95bd2ee024acc6d3382fd3
Chris Turner <chris.j.turner@diamond.ac.uk>
2016-02-05 14:56:06 +0000

    Updated to work the same as the plot directive with options

--- python/zebra2/sphinxext/sequence_plot_directive.py
+++ python/zebra2/sphinxext/sequence_plot_directive.py
@@ -1,6 +1,9 @@
 from docutils import nodes
 
 from docutils.parsers.rst import Directive
+from docutils.parsers.rst import directives
+
+import os
 
 from matplotlib.sphinxext import plot_directive
 
@@ -10,11 +13,30 @@ class sequence_plot_node(nodes.Element):
 
 class sequence_plot_directive(Directive):
 
-    has_content = True
+    has_content = False
+    required_arguments = 0
+    optional_arguments = 0
+    option_spec = {'block': str, 'title': str, 'table': bool}
 
     def run(self):
 
-        pdir = plot_directive.plot_directive(self.name,
+        #fill the content code with the options from the directive
+        blockname = self.options['block']
+        plotname = self.options['title']
+
+        #!?!?!?!MAYBE THIS SHOULD GO IN THE visit_sequence_plot ?
+        if 'table' in self.options:
+            #insert the table
+            print "TABLE", self.options['table']
+        else:
+            print "NO TABLES"
+
+        self.content = [
+            "from block_plot import make_block_plot",
+            "make_block_plot('" + blockname + "',"  + plotname+ " )"]
+
+        #call the plot directive directive to insert the plot
+        plot_directive.plot_directive(self.name,
             self.arguments,
             self.options,
             self.content,
@@ -25,10 +47,9 @@ class sequence_plot_directive(Directive):
             self.state_machine)
 
         text = '\n'.join(self.content)
+        node = sequence_plot_node(rawsource=text, **self.options)
 
-        node = sequence_plot_node(rawsource=text)
-
-        self.state.nested_parse(self.content, self.content_offset, node)
+        # self.state.nested_parse(self.content, self.content_offset, node)
 
         return [node]
 
@@ -40,7 +61,13 @@ def setup(app):
 
 def visit_sequence_plot(self, node):
     #put stuff here to happen before
-    self.body.append("SOME TEXT HERE BEFORE")
+    self.body.append("SOME TEXT HERE BEFORE\n")
+    # self.body.append("======= = = = = = = = = ==== = = = = ==== = = = =")
+    # self.body.append("#       Use Inp Inp Val Ph1  Ph1 Out Ph2  Ph2 Out")
+    # self.body.append("------- ------- ------- ---- ------- ---- -------")
+    # self.body.append("Repeats A B C D A B C D Time A B C D Time A B C D")
+    # self.body.append("======= = = = = = = = = ==== = = = = ==== = = = =")
+    self.body.append(node.attributes['title']+'\n')
 
 def depart_sequence_plot(self, node):
     #put stuff here to happen after
