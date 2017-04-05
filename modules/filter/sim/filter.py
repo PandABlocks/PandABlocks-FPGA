5f9c83769db21a68dd9c8b1dbac6e77200b00ace
Chris Turner <chris.j.turner@diamond.ac.uk>
2017-04-05 13:50:42 +0100

    Add simulation files for FILTER module

---
+++ modules/filter/sim/filter.py
@@ -0,0 +1,19 @@
+from common.python.pandablocks.block import Block
+
+
+class Filter(Block):
+    def __init__(self):
+        self.scale = {0:0, 1:1, 2:2}
+
+    def on_changes(self, ts, changes):
+        """Handle changes at a particular timestamp, then return the timestamp
+        when we next need to be called"""
+        # This is a ConfigBlock object
+        b = self.config_block
+
+        # Set attributes
+        for name, value in changes.items():
+            setattr(self, name, value)
+
+        print changes
+
