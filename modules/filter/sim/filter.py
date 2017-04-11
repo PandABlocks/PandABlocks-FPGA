1ddeda4afec125af8066196d6ba3beeba7fec778
Chris Turner <chris.j.turner@diamond.ac.uk>
2017-04-11 11:02:00 +0100

    Refactoring

--- modules/filter/sim/filter.py
+++ modules/filter/sim/filter.py
@@ -12,11 +12,45 @@ class Filter(Block):
         self.ts_sum_start = 0
         self.ts_sum = 0
 
-    def difference(self):
-        pass
+    def set_values(self, ts):
+        self.OUT = 0
+        if self.MODE == 0:
+            self.set_difference()
+        elif self.MODE == 1:
+            self.set_avearage(ts)
 
-    def average(self):
-        pass
+    def set_difference(self):
+        self.latch = self.INP
+
+    def set_avearage(self, ts):
+        self.nsamples = 0
+        self.sum = 0
+        self.ts_sum_start = ts
+        self.ts_sum = ts
+
+    def handle_trig(self, ts):
+        if self.MODE == 0:
+            self.difference(ts)
+        elif self.MODE == 1:
+            self.average(ts)
+
+    def difference(self, ts):
+        self.OUT = self.INP-self.latch
+        self.latch = self.INP
+        self.READY = 1
+        self.queue.append((ts + 1, 0))
+
+    def average(self, ts):
+        self.nsamaples = ts - self.ts_sum_start
+        self.sum += self.INP * (ts - self.ts_sum)
+        self.OUT = self.sum / (self.nsamaples + 1)
+
+    def do_sum(self, ts):
+        self.sum += self.INP
+        self.ts_sum = ts
+        if sum > (2**32 - 1):
+            self.ERR = 1
+            self.ENABLE == 0
 
     def on_changes(self, ts, changes):
         """Handle changes at a particular timestamp, then return the timestamp
@@ -30,37 +64,15 @@ class Filter(Block):
 
         # Set values on rising edge of ENABLE
         if changes.get(b.ENABLE) == 1:
-            self.OUT = 0
-            if self.MODE == 0:
-                self.latch = self.INP
-            elif self.MODE == 1:
-                self.nsamples = 0
-                self.sum = 0
-                self.ts_sum_start = ts
-                self.ts_sum = ts
-                pass
+            self.set_values(ts)
 
         # Do actions on rising edge of TRIG
         if changes.get(b.TRIG) == 1:
             if self.ENABLE == 1:
-                if self.MODE == 0:
-                    self.OUT = self.INP-self.latch
-                    self.latch = self.INP
-                    self.READY = 1
-                    self.queue.append((ts + 1, 0))
-                elif self.MODE == 1:
-                    self.nsamaples = ts - self.ts_sum_start
-                    self.sum += self.INP * (ts - self.ts_sum)
-                    self.OUT = self.sum / (self.nsamaples + 1)
-                    pass
+                self.handle_trig(ts)
 
         if b.INP in changes:
-            self.sum += self.INP
-            self.ts_sum = ts
-
-            if sum > (2**32 - 1):
-                self.ERR = 1
-                #stop processing
+            self.do_sum(ts)
 
         # End the 1 cycle pulse on ready
         if self.queue and self.queue[0][0] == ts:
