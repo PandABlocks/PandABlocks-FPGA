474b444d75c93a8806348d54f2890ef19ed8463c
Chris Turner <chris.j.turner@diamond.ac.uk>
2017-04-11 16:54:06 +0100

    Fix averaging mode

--- modules/filter/sim/filter.py
+++ modules/filter/sim/filter.py
@@ -11,6 +11,7 @@ class Filter(Block):
         self.queue = deque()
         self.ts_sum_start = 0
         self.ts_sum = 0
+        self.inp_prev = 0
 
     def set_values(self, ts):
         self.OUT = 0
@@ -27,6 +28,7 @@ class Filter(Block):
         self.sum = 0
         self.ts_sum_start = ts
         self.ts_sum = ts
+        self.inp_prev = self.INP
 
     def handle_trig(self, ts):
         if self.MODE == 0:
@@ -41,13 +43,14 @@ class Filter(Block):
         self.queue.append((ts + 1, 0))
 
     def average(self, ts):
-        self.nsamaples = ts - self.ts_sum_start
+        self.nsamples = ts - self.ts_sum_start
         self.sum += self.INP * (ts - self.ts_sum)
-        self.OUT = self.sum / (self.nsamaples + 1)
+        self.OUT = self.sum / (self.nsamples)
 
     def do_sum(self, ts):
-        self.sum += self.INP
+        self.sum += self.inp_prev * (ts - self.ts_sum - 1) + self.INP
         self.ts_sum = ts
+        self.inp_prev = self.INP
         if sum > (2**32 - 1):
             self.ERR = 1
             self.ENABLE == 0
@@ -66,13 +69,13 @@ class Filter(Block):
         if changes.get(b.ENABLE) == 1:
             self.set_values(ts)
 
+        if b.INP in changes:
+            self.do_sum(ts)
+
         # Do actions on rising edge of TRIG
         if changes.get(b.TRIG) == 1:
             if self.ENABLE == 1:
                 self.handle_trig(ts)
-
-        if b.INP in changes:
-            self.do_sum(ts)
 
         # End the 1 cycle pulse on ready
         if self.queue and self.queue[0][0] == ts:
