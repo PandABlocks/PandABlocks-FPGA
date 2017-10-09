790db0d41ec283dca5e68c6a7408e34966332650
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-10-09 13:28:36 +0100

    Add PCOMP simulation to support DIR=EITHER and write lots of docs

--- modules/pcomp/sim/pcomp.py
+++ modules/pcomp/sim/pcomp.py
@@ -1,25 +1,153 @@
 from common.python.pandablocks.block import Block
-import os
-import csv
+
+import numpy as np
+
+# state machine states
+WAIT_ENABLE = 0
+WAIT_DIR = 1
+WAIT_PRE_START = 2
+WAIT_RISING = 3
+WAIT_FALLING = 4
 
 # for directions
-FWD = 0
-BWD = 1
+POSITIVE = 0
+NEGATIVE = 1
+EITHER = 2
+
+# for errors
+OK = 0
+ERR_PJUMP = 1
+ERR_GUESS = 2
 
 
 class Pcomp(Block):
 
     def __init__(self):
-        # Next compare point
-        self.cpoint = 0
-        # Next compare output
-        self.cout = 1
-        # Produced pulses
-        self.cnum = 0
-        # State to capture waiting before the position crosses the start point
-        self.wait_start = True
-        # flag an error on next ts?
-        self.flag_error = False
+        # The sign multiplier for direction, None for not guessed yet
+        self.mult = None
+        # The position that we started at
+        self.initial = 0
+        # The inp value at the start of the current pulse
+        self.current_pulse = None
+
+    def do_enable(self):
+        # When enabled, reset outputs and readbacks
+        self.ACTIVE = 1
+        self.OUT = 0
+        self.HEALTH = OK
+        self.PRODUCED = 0
+        # Latch the position when we started
+        self.initial = self.INP
+        self.current_pulse = None
+        # Direction is specified or guessed
+        if self.DIR == POSITIVE:
+            self.mult = 1
+        elif self.DIR == NEGATIVE:
+            self.mult = -1
+        elif self.DIR == EITHER:
+            self.mult = None
+            return WAIT_DIR
+        # Return the initial state
+        return WAIT_PRE_START
+
+    def do_disable(self, health=OK):
+        # When disabled, set outputs but leave readbacks
+        self.ACTIVE = 0
+        self.OUT = 0
+        self.HEALTH = health
+        return WAIT_ENABLE
+
+    def reached_thresh(self, inp, threshold):
+        return self.mult * (inp - threshold) >= 0
+
+    def produce_rising(self, current_pulse):
+        falling = current_pulse + self.mult * self.WIDTH
+        if self.reached_thresh(self.INP, falling):
+            return self.do_disable(ERR_PJUMP)
+        else:
+            self.OUT = 1
+            self.PRODUCED += 1
+            self.current_pulse = current_pulse
+            return WAIT_FALLING
+
+    def produce_falling(self):
+        self.OUT = 0
+        if self.PRODUCED == self.PULSES:
+            self.ACTIVE = 0
+            return WAIT_ENABLE
+        else:
+            rising = self.current_pulse + self.mult * self.STEP
+            if self.reached_thresh(self.INP, rising):
+                return self.do_disable(ERR_PJUMP)
+            else:
+                return WAIT_RISING
+
+    def pre_start_thresh(self):
+        if self.RELATIVE:
+            thresh = self.initial + self.mult * (self.START - self.PRE_START)
+        else:
+            thresh = self.START - self.mult * self.PRE_START
+        return thresh
+
+    def do_check_dir(self):
+        if self.RELATIVE:
+            # If we are relative then only advance when thresh away from
+            # the initial position
+            sign = 0
+            if self.PRE_START > 0:
+                if abs(self.INP - self.initial) >= self.PRE_START:
+                    # The sign is the other way from where we are going
+                    sign = np.sign(self.initial - self.INP)
+            elif self.START > 0:
+                if abs(self.INP - self.initial) >= self.START:
+                    sign = np.sign(self.INP - self.initial)
+            else:
+                return self.do_disable(ERR_GUESS)
+        else:
+            sign = np.sign(self.START - self.INP)
+        if sign != 0:
+            self.mult = sign
+            if self.PRE_START > 0:
+                return WAIT_PRE_START
+            else:
+                return WAIT_RISING
+        else:
+            # Wait for a bit
+            return WAIT_DIR
+
+    def do_pre_start_check(self):
+        # Check we have crossed thresh IN OPPOSITE DIRECTION
+        if self.mult * (self.INP - self.pre_start_thresh()) < 0:
+            return WAIT_RISING
+        else:
+            return WAIT_PRE_START
+
+    def rising_thresh(self):
+        if self.current_pulse is None:
+            if self.RELATIVE:
+                thresh = self.initial + self.mult * self.START
+            else:
+                thresh = self.START
+        else:
+            thresh = self.current_pulse + self.mult * self.STEP
+        return thresh
+
+    def do_rising_check(self):
+        thresh = self.rising_thresh()
+        if self.reached_thresh(self.INP, thresh):
+            return self.produce_rising(thresh)
+        else:
+            return WAIT_RISING
+
+    def falling_thresh(self):
+        return self.current_pulse + self.mult * self.WIDTH
+
+    def do_falling_check(self):
+        thresh = self.falling_thresh()
+        if self.reached_thresh(self.INP, thresh):
+            return self.produce_falling()
+        else:
+            return WAIT_FALLING
 
     def on_changes(self, ts, changes):
         """Handle changes at a particular timestamp, then return the timestamp
@@ -31,66 +159,26 @@ class Pcomp(Block):
         for name, value in changes.items():
             setattr(self, name, value)
 
-        if self.flag_error:
-            self.ERROR = 1
-            self.ACTIVE = 0
-            self.OUT = 0
-            self.flag_error = False
-
-        # handle enable transitions
+        # Handle enable transitions
         if b.ENABLE in changes:
             if self.ENABLE:
-                self.ACTIVE = 1
-                # if relative then start position is from current pos
-                if self.RELATIVE:
-                    self.cpoint = self.START + self.INP
-                else:
-                    self.cpoint = self.START
-                self.wait_start = True
-                self.cnum = 0
-                self.ERROR = 0
+                # If we get enabled then start listening for points
+                self.STATE = self.do_enable()
             else:
-                self.ACTIVE = 0
-                self.OUT = 0
+                self.STATE = self.do_disable()
 
-        # check to see if we are waiting to cross the start point
         if self.ACTIVE:
-            if self.DIR == FWD and self.INP < self.cpoint - self.DELTAP or \
-                    self.DIR == BWD and self.INP > self.cpoint + self.DELTAP:
-                self.wait_start = False
+            if self.STATE == WAIT_DIR:
+                # Check when we have passed START - PRE_START
+                self.STATE = self.do_check_dir()
 
-            # Handle pulses
-            if self.DIR == FWD:
-                transition = self.INP >= self.cpoint
-            else:
-                transition = self.INP <= self.cpoint
-            if transition and not self.wait_start:
-                if self.OUT == 0:
-                    if self.DIR == FWD:
-                        self.cpoint += self.WIDTH
-                        if self.INP > self.cpoint:
-                            self.flag_error = True
-                            return ts + 1                            
-                    else:
-                        self.cpoint -= self.WIDTH                    
-                        if self.INP < self.cpoint:
-                            self.flag_error = True
-                            return ts + 1                                                    
-                    self.OUT = 1
-                    self.cnum += 1
-                else:
-                    if self.DIR == FWD:
-                        self.cpoint += self.STEP - self.WIDTH
-                        if self.INP > self.cpoint:
-                            self.flag_error = True
-                            return ts + 1                           
-                    else:
-                        self.cpoint -= self.STEP - self.WIDTH
-                        if self.INP < self.cpoint:
-                            self.flag_error = True
-                            return ts + 1                           
-                    self.OUT = 0
-                    # if we've done PNUM, then stop
-                    if self.cnum >= self.PNUM:
-                        self.ACTIVE = 0
+            if self.STATE == WAIT_PRE_START:
+                # Check when we have passed START - PRE_START
+                self.STATE = self.do_pre_start_check()
 
+            if self.STATE == WAIT_RISING:
+                # Check when we have passed START
+                self.STATE = self.do_rising_check()
+            elif self.STATE == WAIT_FALLING:
+                # Check when we have passed WIDTH
+                self.STATE = self.do_falling_check()
