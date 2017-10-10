9a6f410df7a70550fb68500739e14a49edfceba5
Paul Garrick <paul.garrick@diamond.ac.uk>
2017-10-10 11:37:36 +0100

    Modify PCOMP sim to look a bit more like a VHDL FSM

--- modules/pcomp/sim/pcomp.py
+++ modules/pcomp/sim/pcomp.py
@@ -60,7 +60,7 @@ class Pcomp(Block):
     def reached_thresh(self, inp, threshold):
         return self.mult * (inp - threshold) >= 0
 
-    def produce_rising(self, current_pulse):
+    def do_produce_rising(self, current_pulse):
         falling = current_pulse + self.mult * self.WIDTH
         if self.reached_thresh(self.INP, falling):
             return self.do_disable(ERR_PJUMP)
@@ -70,7 +70,7 @@ class Pcomp(Block):
             self.current_pulse = current_pulse
             return WAIT_FALLING
 
-    def produce_falling(self):
+    def do_produce_falling(self):
         self.OUT = 0
         if self.PRODUCED == self.PULSES:
             self.ACTIVE = 0
@@ -93,27 +93,23 @@ class Pcomp(Block):
         if self.RELATIVE:
             # If we are relative then only advance when thresh away from
             # the initial position
-            sign = 0
             if self.PRE_START > 0:
-                if abs(self.INP - self.initial) >= self.PRE_START:
+                if abs(self.INP - self.initial) > self.PRE_START:
                     # The sign is the other way from where we are going
-                    sign = np.sign(self.initial - self.INP)
+                    self.mult = np.sign(self.initial - self.INP)
+                    return WAIT_RISING                    
             elif self.START > 0:
                 if abs(self.INP - self.initial) >= self.START:
-                    sign = np.sign(self.INP - self.initial)
+                    self.mult = np.sign(self.INP - self.initial)
+                    return self.do_rising_check()
             else:
                 return self.do_disable(ERR_GUESS)
         else:
             sign = np.sign(self.START - self.INP)
-        if sign != 0:
-            self.mult = sign
-            if self.PRE_START > 0:
+            if sign != 0:
+                self.mult = sign
                 return WAIT_PRE_START
-            else:
-                return WAIT_RISING
-        else:
-            # Wait for a bit
-            return WAIT_DIR
+        return WAIT_DIR
 
     def do_pre_start_check(self):
         # Check we have crossed thresh IN OPPOSITE DIRECTION
@@ -135,7 +131,7 @@ class Pcomp(Block):
     def do_rising_check(self):
         thresh = self.rising_thresh()
         if self.reached_thresh(self.INP, thresh):
-            return self.produce_rising(thresh)
+            return self.do_produce_rising(thresh)
         else:
             return WAIT_RISING
 
@@ -145,7 +141,7 @@ class Pcomp(Block):
     def do_falling_check(self):
         thresh = self.falling_thresh()
         if self.reached_thresh(self.INP, thresh):
-            return self.produce_falling()
+            return self.do_produce_falling()
         else:
             return WAIT_FALLING
 
@@ -160,25 +156,31 @@ class Pcomp(Block):
             setattr(self, name, value)
 
         # Handle enable transitions
+        state = None
+
         if b.ENABLE in changes:
             if self.ENABLE:
                 # If we get enabled then start listening for points
-                self.STATE = self.do_enable()
+                state = self.do_enable()
             else:
-                self.STATE = self.do_disable()
-
-        if self.ACTIVE:
+                state = self.do_disable()
+        elif self.ACTIVE:
             if self.STATE == WAIT_DIR:
                 # Check when we have passed START - PRE_START
-                self.STATE = self.do_check_dir()
-
-            if self.STATE == WAIT_PRE_START:
+                state = self.do_check_dir()
+                
+            elif self.STATE == WAIT_PRE_START:
                 # Check when we have passed START - PRE_START
-                self.STATE = self.do_pre_start_check()
-
-            if self.STATE == WAIT_RISING:
+                state = self.do_pre_start_check()
+            elif self.STATE == WAIT_RISING:
                 # Check when we have passed START
-                self.STATE = self.do_rising_check()
+                state = self.do_rising_check()
             elif self.STATE == WAIT_FALLING:
                 # Check when we have passed WIDTH
-                self.STATE = self.do_falling_check()
+                state = self.do_falling_check()
+        
+        if state is not None:
+            self.STATE = state
+            # We changed state, might need another transition next clock tick
+            return ts + 1
+
