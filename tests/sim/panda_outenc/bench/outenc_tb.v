4346436d2f78b370aedebd6417063bfbab34f3e4
Paul Garrick <paul.garrick@diamond.ac.uk>
2018-07-17 07:47:13 +0100

    Added ssi_master and biss_master components to test bench

--- tests/sim/panda_outenc/bench/outenc_tb.v
+++ tests/sim/panda_outenc/bench/outenc_tb.v
@@ -2,6 +2,8 @@
 
 module outenc_tb;
 
+parameter C_MODE = 1;
+
 // Inputs
 reg         clk_i = 0;
 reg         reset_i;
@@ -9,20 +11,38 @@ reg         a_ext_i;
 reg         b_ext_i;
 reg         z_ext_i;
 reg         data_ext_i;
-reg [31:0]  posn_i;
+reg [31:0]  posn_i = 32'hFFFFFFFF;
 reg         enable_i;
 reg [2:0]   PROTOCOL;
-reg         BYPASS;
-reg [7:0]   BITS;
+//reg         BYPASS = 0;
+reg [1:0]   MODE = C_MODE;
+reg [7:0]   BITS = 32;
 reg [31:0]  QPERIOD;
+
+reg  [31:0] CLK_PERIOD = 100;
+reg  [31:0] FRAME_PERIOD = 50;
+reg         ssi_dat_i;
 
 // Outputs
 wire        A_OUT;
 wire        B_OUT;
 wire        Z_OUT;
 wire        DATA_OUT;
-reg         CLK_IN = 0;
+reg         CLK_IN;
 wire [31:0] QSTATE; 
+
+wire        ssi_sck_o;
+wire [31:0] ssi_posn_o;
+wire        ssi_posn_valid_o;        
+
+
+wire        biss_sck_o;
+reg         biss_dat_i;
+wire [31:0] biss_posn_o;
+wire        biss_posn_valid_o;
+
+reg         biss_not_ssi = C_MODE[0];
+  
 
 always #4 clk_i = !clk_i;
 
@@ -44,7 +64,8 @@ outenc uut (
         .CLK_IN         ( CLK_IN        ),        
 
         .PROTOCOL       ( PROTOCOL      ), 
-        .BYPASS         ( BYPASS        ),   
+//        .BYPASS         ( BYPASS        ),   
+        .MODE           ( MODE          ),
         .BITS           ( BITS          ), 
         .QPERIOD        ( QPERIOD       ),
         .QPERIOD_WSTB   ( QPERIOD_WSTB  ),
@@ -52,23 +73,61 @@ outenc uut (
 );
 
 
-//
-//QUAD RECEIVER
-//
-//wire [31: 0]    quadin_posn;
 
-//quadin quadin (
-//    .clk_i        ( clk_i               ),
-//    .reset_i      ( reset_i             ),
-//    .a_ext_i      ( a_o                 ),
-//    .b_ext_i      ( b_o                 ),
-//    .z_ext_i      ( 1'b0                ),
-//    .rst_z_i      ( 1'b0                ),
-//    .setp_val_i   ( 0                   ),
-//    .setp_wstb_i  ( 1'b0                ),
-//    .posn_o       ( quadin_posn         )
-//);
+//assign CLK_IN = ssi_sck_o;
+//assign ssi_dat_i = DATA_OUT;
 
+always @(biss_not_ssi, DATA_OUT, ssi_sck_o, biss_sck_o)
+begin
+    if (biss_not_ssi == 0) begin
+        CLK_IN = ssi_sck_o;
+        ssi_dat_i = DATA_OUT;
+    end else begin
+        CLK_IN = biss_sck_o;
+        biss_dat_i = DATA_OUT;
+   end
+end            
+
+
+always @(biss_not_ssi, ssi_posn_valid_o, biss_posn_valid_o)
+begin
+    if (biss_not_ssi == 0) begin
+        if (ssi_posn_valid_o == 1) begin
+            posn_i = posn_i + 32'ha5a5a5a5;        
+        end
+    end else begin
+        if (biss_posn_valid_o == 1) begin
+            posn_i = posn_i + 32'ha5a5a5a5;
+        end 
+    end
+end             
+    
+
+
+ssi_master ssi_master_uut(
+        .clk_i          ( clk_i             ),
+        .reset_i        ( reset_i           ),
+        .BITS           ( BITS              ),
+        .CLK_PERIOD     ( CLK_PERIOD        ),
+        .FRAME_PERIOD   ( FRAME_PERIOD      ),
+        .ssi_sck_o      ( ssi_sck_o         ),
+        .ssi_dat_i      ( ssi_dat_i         ),
+        .posn_o         ( ssi_posn_o        ),
+        .posn_valid_o   ( ssi_posn_valid_o  )
+);                 
+
+
+biss_master biss_master_uut(
+        .clk_i          ( clk_i             ),
+        .reset_i        ( reset_i           ),
+        .BITS           ( BITS              ),
+        .CLK_PERIOD     ( CLK_PERIOD        ),
+        .FRAME_PERIOD   ( FRAME_PERIOD      ),
+        .biss_sck_o     ( biss_sck_o        ),
+        .biss_dat_i     ( biss_dat_i        ),
+        .posn_o         ( biss_posn_o       ),
+        .posn_valid_o   ( biss_posn_valid_o )
+);         
 
 
 initial begin
@@ -80,29 +139,28 @@ initial begin
     posn_i = 0;
     enable_i = 0;
     PROTOCOL = 0;
-    BYPASS = 0;
-    BITS = 0;
     QPERIOD = 125;
 
     repeat(100) @(posedge clk_i);
     reset_i = 0;
+        
     repeat(1250) @(posedge clk_i);
 
     repeat(125) @(posedge clk_i);
     enable_i = 1;
     repeat(125) @(posedge clk_i);
-    posn_i = 1000;
+//    posn_i = 1000;
     repeat(1250) @(posedge clk_i);
-    posn_i = 7;
+//    posn_i = 7;
     //
     repeat(1250) @(posedge clk_i);
-    posn_i = 21;
+//    posn_i = 21;
 
     repeat(1250) @(posedge clk_i);
-    posn_i = 200;
+//    posn_i = 200;
 
     repeat(1250) @(posedge clk_i);
-    posn_i = 9;
+//    posn_i = 9;
 
     
     repeat(12500) @(posedge clk_i);
