03f3370c782ad60b8d26d8b447f5ac2509c4ba4f
paul garrick <paul.garrick@diamond.ac.uk>
2018-10-05 10:45:42 +0100

    Panda acq427 changes Separated the ADC and DAC into two separate modules

---
+++ modules/adc/vhdl/fmc_adc427_top.vhd
@@ -0,0 +1,257 @@
+--------------------------------------------------------------------------------
+--  PandA Motion Project - 2017,18
+--      Diamond Light Source, Oxford, UK
+--      SOLEIL Synchrotron, GIF-sur-YVETTE, France
+--
+--  Author      : Scott Robson (D-TACQ Solutions)
+--------------------------------------------------------------------------------
+--
+--  Description : FMC ACQ427 module interface to D-TACQ ACQ427FMC Module
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+library work;
+use work.support.all;
+use work.top_defines.all;
+
+entity fmc_adc427_top is
+port (
+    -- Clock and Reset
+    clk_i               : in  std_logic;
+    reset_i             : in  std_logic;
+    -- Bus Inputs
+    bitbus_i            : in  std_logic_vector(127 downto 0);
+    posbus_i            : in  std32_array(31 downto 0);
+    -- Generic Inputs to BitBus and PosBus from FMC and SFP
+    --fmc_inputs_o        : out std_logic_vector(15 downto 0);
+    fmc_data_o          : out std32_array(15 downto 0); -- 8 channels of 32-bit data
+    -- Memory Bus Interface
+    read_strobe_i       : in  std_logic;
+    read_address_i      : in  std_logic_vector(PAGE_AW-1 downto 0);
+    read_data_o         : out std_logic_vector(31 downto 0);
+    read_ack_o          : out std_logic;
+
+    write_strobe_i      : in  std_logic;
+    write_address_i     : in  std_logic_vector(PAGE_AW-1 downto 0);
+    write_data_i        : in  std_logic_vector(31 downto 0);
+    write_ack_o         : out std_logic;
+    -- External Differential Clock (via front panel SMA)
+    FMC_LA_P            : inout std_logic_vector(33 downto 0) := (others => 'Z');
+    FMC_LA_N            : inout std_logic_vector(33 downto 0) := (others => 'Z')
+);
+end fmc_adc427_top;
+
+architecture rtl of fmc_adc427_top is
+
+---------------------------------------------------------------------------------------
+-- FMC pin name translation signals.
+---------------------------------------------------------------------------------------
+signal p_ADC_CNV_A          : std_logic                     := '0';             --! ADC Convert Control A
+signal p_ADC_CNV_B          : std_logic                     := '0';             --! ADC Convert Control B
+signal p_ADC_SPI_CLK        : std_logic                     := '0';             --! ADC SPI Clock
+signal p_ADC_SDO            : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data
+signal p_FMC_EXT_CLK        : std_logic;                                        --! Sample Clock from ACQ420FMC
+signal p_FMC_EXT_TRIG       : std_logic;                                        --! Trigger from ACQ420FMC
+
+-- Internal Names
+signal ADC_CNV              : std_logic                     := '0';             --! ADC Convert Control
+signal ADC_SPI_CLK          : std_logic                     := '0';             --! ADC SPI Clock
+signal ADC_SDO              : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data
+signal FMC_EXT_CLK          : std_logic;                                        --! Sample Clock from ACQ420FMC
+signal FMC_EXT_TRIG         : std_logic;                                        --! Trigger from ACQ420FMC
+signal FMC_IO_BUS           : std_logic_vector(3 downto 0)  := (others => '0'); --! FMC IO Controls (CLOCK_DAT,CLOCK_DIR,TRIG_DAT,TRIG_DIR)
+
+signal FMC_MODULE_ENABLE_n  : std_logic;                                        --! FPGA Enable Outputs
+signal MODULE_ENABLE        : std_logic_vector(31 downto 0);                    --! FPGA Enable Outputs
+signal ADC_CLK_SELECT       : std_logic_vector(31 downto 0);
+signal ADC_CLKDIV           : std_logic_vector(31 downto 0);
+signal ADC_FIFO_RESET       : std_logic_vector(31 downto 0);
+signal ADC_FIFO_ENABLE      : std_logic_vector(31 downto 0);
+signal ADC_RESET            : std_logic_vector(31 downto 0);
+signal ADC_ENABLE           : std_logic_vector(31 downto 0);
+
+---------------------------------------------------------------------------------------
+-- ADC I/O Logic
+---------------------------------------------------------------------------------------
+
+signal clk_ADC_IOB          : std_logic;                                        --! ADC SPI Clk for IOBs
+
+signal s_ADC_SPI_CLK        : std_logic;                                        --! ADC SPI Clock
+signal s_ADC_SDO            : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data
+signal s_ADC_SDO_IDELAY     : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data Idelay
+signal s_ADC_SDO_d1         : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data
+signal s_ADC_SDO_d2         : std_logic_vector( 8 downto 1) := (others => '0'); --! ADC SPI Data
+
+signal s_ADC_SPI_CLK_STOP   : std_logic;                                        --! Stop the ADC SPI Clock
+
+signal SPI_IDELAY_DATA      : std_logic_vector(4 downto 0);                     --! IDELAY Tap Delay Value
+signal SPI_IDELAY_LD        : std_logic;                                        --! IDELAY Load Tap Delay Value
+
+signal s_TRIG_DATA          : std_logic := '0';                                 --! External Trigger Data
+signal s_CLOCK_DATA         : std_logic := '0';                                 --! External Clock Data
+
+signal ADC_DATAOUT          : std_logic_vector(255 downto 0) := (others => '0');
+
+
+---------------------------------------------------------------------------------------
+-- Signal Attributes
+---------------------------------------------------------------------------------------
+attribute mark_debug    : string;
+attribute keep          : string;
+attribute IOB           : string;
+
+
+-- ADC Attributes
+attribute keep      of ADC_SDO          : signal is "true";
+attribute IOB       of s_ADC_SDO        : signal is "true";
+
+attribute keep      of s_ADC_SPI_CLK    : signal is "true";
+attribute IOB       of ADC_SPI_CLK      : signal is "true";
+
+--------------------------------------------------------------------------------------
+-- debug test using mark_debug
+---------------------------------------------------------------------------------------
+--attribute mark_debug of s_ADC_SDO       : signal is "true";
+
+begin
+
+-- Acknowledgement to AXI Lite interface
+write_ack_o <= '1';
+
+---------------------------------------------------------------------------------------
+-- Translate the FMC pin names into ACQ427FMC names
+---------------------------------------------------------------------------------------
+-- ADC
+---------------------------------------------------------------------------------------
+-- Input Pins
+p_FMC_EXT_CLK  <=  FMC_LA_P(0);
+p_FMC_EXT_TRIG <=  FMC_LA_P(12);
+p_ADC_SDO(8)   <=  FMC_LA_P(20);
+p_ADC_SDO(7)   <=  FMC_LA_P(21);
+p_ADC_SDO(6)   <=  FMC_LA_P(22);
+p_ADC_SDO(5)   <=  FMC_LA_P(23);
+p_ADC_SDO(4)   <=  FMC_LA_P(16);
+p_ADC_SDO(3)   <=  FMC_LA_P(17);
+p_ADC_SDO(2)   <=  FMC_LA_P(18);
+p_ADC_SDO(1)   <=  FMC_LA_P(19);
+
+-- Output Pins
+FMC_LA_P(14)   <=  p_ADC_CNV_A;
+FMC_LA_P(15)   <=  p_ADC_CNV_B;
+FMC_LA_P(13)   <=  p_ADC_SPI_CLK;
+
+
+s_TRIG_DATA    <=  FMC_IO_BUS(1);
+s_CLOCK_DATA   <=  FMC_IO_BUS(3);
+
+
+---------------------------------------------------------------------------------------
+-- IO Buffer Instantiation
+---------------------------------------------------------------------------------------
+-- ADC
+---------------------------------------------------------------------------------------
+cmp_FMC_ADC_TRIG:       IOBUF port map(IO => p_FMC_EXT_TRIG,I => s_TRIG_DATA,  O => FMC_EXT_TRIG, T => not FMC_IO_BUS(0));
+cmp_FMC_ADC_CLK:        IOBUF port map(IO => p_FMC_EXT_CLK, I => s_CLOCK_DATA, O => FMC_EXT_CLK,  T => not FMC_IO_BUS(2));
+
+cmp_ADC_CNV_A:          IOBUF port map(IO => p_ADC_CNV_A, I => ADC_CNV, T => FMC_MODULE_ENABLE_n);
+cmp_ADC_CNV_B:          IOBUF port map(IO => p_ADC_CNV_B, I => ADC_CNV, T => FMC_MODULE_ENABLE_n);
+
+cmp_ADC_CLK:        OBUFT generic map (SLEW => "FAST") port map(O => p_ADC_SPI_CLK, I => ADC_SPI_CLK, T => FMC_MODULE_ENABLE_n);
+
+gen_ADC_BUFS: for x in 1 to 8 generate
+    cmp_ADC_SDO: IBUF port map(I => p_ADC_SDO(x), O => ADC_SDO(x));
+end generate gen_ADC_BUFS ;
+
+
+
+-- Unused IO
+FMC_LA_P(33 downto 24)  <= (others => 'Z');
+FMC_LA_N(33 downto 0)   <= (others => 'Z');
+
+fmc_ctrl : entity work.adc_ctrl
+port map (
+    -- Clock and Reset
+    clk_i               => clk_i,
+    reset_i             => reset_i,
+    sysbus_i            => bitbus_i,
+    posbus_i            => posbus_i,
+    -- Memory Bus Interface
+    read_strobe_i       => read_strobe_i,
+    read_address_i      => read_address_i(BLK_AW-1 downto 0),
+    read_data_o         => read_data_o,
+    read_ack_o          => open,
+
+    write_strobe_i      => write_strobe_i,
+    write_address_i     => write_address_i(BLK_AW-1 downto 0),
+    write_data_i        => write_data_i,
+    write_ack_o         => open
+);
+
+
+fmc_adc_start : entity work.fmc_adc_start
+port map (
+    clk_i               => clk_i,
+    reset_i             => reset_i,
+    MODULE_ENABLE       => MODULE_ENABLE,
+    ADC_CLK_SELECT      => ADC_CLK_SELECT,
+    ADC_CLKDIV          => ADC_CLKDIV,
+    ADC_FIFO_RESET      => ADC_FIFO_RESET,
+    ADC_FIFO_ENABLE     => ADC_FIFO_ENABLE,
+    ADC_RESET           => ADC_RESET,
+    ADC_ENABLE          => ADC_ENABLE
+);
+
+            
+
+FMC_MODULE_ENABLE_n <= not MODULE_ENABLE(0);
+
+THE_ACQ427FMC_ADC_INTERFACE : entity work.ACQ427FMC_ADC_INTERFACE
+port map (
+    clk_PANDA               => clk_i,           -- 100 MHz Clock from ARM for ADC Timing
+    EXT_CLOCK               => FMC_EXT_CLK,     -- External Clock Source
+    FMC_IO_BUS              => FMC_IO_BUS,      -- FMC IO Controls (CLOCK_DAT,CLOCK_DIR,TRIG_DAT,TRIG_DIR)
+    ADC_CLK_SELECT_REG      => ADC_CLK_SELECT,
+    ADC_CLKDIV_REG          => ADC_CLKDIV,
+    ADC_FIFO_RESET_REG      => ADC_FIFO_RESET,
+    ADC_FIFO_ENABLE_REG     => ADC_FIFO_ENABLE,
+    ADC_RESET_REG           => ADC_RESET,
+    ADC_ENABLE_REG          => ADC_ENABLE,
+    clk_ADC_IOB             => clk_ADC_IOB,     -- ADC SPI Clock domain for IOBs
+    ADC_SDO                 => s_ADC_SDO,       -- ADC SPI Data
+    ADC_CNV                 => ADC_CNV,         -- ADC Convert Control
+    ADC_SPI_CLK             => s_ADC_SPI_CLK,   -- ADC SPI Clock
+    -- Panda Output
+    ADC_DATAOUT             =>  ADC_DATAOUT
+    );
+
+fmc_data_o(7) <= ADC_DATAOUT(31 downto 0);
+fmc_data_o(6) <= ADC_DATAOUT(63 downto 32);
+fmc_data_o(5) <= ADC_DATAOUT(95 downto 64);
+fmc_data_o(4) <= ADC_DATAOUT(127 downto 96);
+fmc_data_o(3) <= ADC_DATAOUT(159 downto 128);
+fmc_data_o(2) <= ADC_DATAOUT(191 downto 160);
+fmc_data_o(1) <= ADC_DATAOUT(223 downto 192);
+fmc_data_o(0) <= ADC_DATAOUT(255 downto 224);
+
+
+IOB_FF_PUSH_ADC: process(clk_ADC_IOB)
+begin
+if Rising_Edge(clk_ADC_IOB) then
+    ADC_SPI_CLK     <= s_ADC_SPI_CLK;
+    if s_ADC_SPI_CLK = '1' then -- Only latch in on approaching rising edge
+        s_ADC_SDO       <= ADC_SDO;
+    end if;
+end if;
+end process IOB_FF_PUSH_ADC;
+
+
+
+end rtl;
+
---
+++ modules/dac/vhdl/fmc_dac_start.vhd
@@ -0,0 +1,144 @@
+--Scott Robson <scott.robson@d-tacq.co.uk>
+--11:59:51 Fri 19 Jan 2018
+--Order of operations to begin data output on ACQ427 DAC Module
+--
+--  Set a suitable clock divider.
+--    We divide PandA clk_0 by 2 to get the source clock for 427 DAC logic. Max sample rate of DAC is 1 MHz.
+--    
+--    Example of clock configuration
+--    
+--    Target update rate of 1 MHz
+--    Internal ACQ427 DAC clock = 62.5 MHz
+--    Set DAC clock divide to 62, 62.5M / 62 ~= 1 MHz
+--    
+--  Source DAC data from counter
+--    Set clock A period to 1e-6 and connect to counters block TRIG port
+--    Set FMC Channel Data to "COUNTER1.OUT"
+--    Set a sensible counter STEP
+--    
+--  Set configuration bits in the following order
+--    Set MODULE_ENABLE
+--    Set DAC_CLKDIV (62)
+--    --N.B. The following 4 steps are required on every power cycle. The act of asserting and de-asserting reset performs some initialisation SPI writes to the DACs
+--    Ensure DAC_ENABLE is Disabled
+--    Assert then Deassert DAC_RESET
+--    Set DAC_ENABLE
+--    Set DAC_FIFO_ENABLE
+--
+--  Data should now be updating on the ACQ427 outputs.
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+entity fmc_dac_start is
+    port (clk_i             : in  std_logic;
+          reset_i           : in  std_logic;
+          MODULE_ENABLE     : out std_logic_vector(31 downto 0);
+          DAC_CLKDIV        : out std_logic_vector(31 downto 0);
+          DAC_FIFO_RESET    : out std_logic_vector(31 downto 0);
+          DAC_FIFO_ENABLE   : out std_logic_vector(31 downto 0);
+          DAC_RESET         : out std_logic_vector(31 downto 0);
+          DAC_ENABLE        : out std_logic_vector(31 downto 0)
+);
+end fmc_dac_start;
+
+architecture rtl of fmc_dac_start is
+
+-- I know why on earth have i put such a long wait and reset here put i tried smaller ones
+-- and the block didn't work it did in simulation but not on the hardware there must be some
+-- issue with the hardware that requires a long time period. 
+constant c_dac_reset_wait  : unsigned(26 downto 0) := to_unsigned(125000000,27);
+constant c_dac_enable_wait : unsigned(26 downto 0) := to_unsigned(125000000,27); 
+
+type t_sm_dac_start is (state_dac_start, state_dac_module_enable ,state_dac_clkdiv, state_dac_reset_en, state_dac_reset_dis, state_dac_enable, state_dac_fifo_enable);
+
+signal sm_dac_start : t_sm_dac_start;
+signal wait_cnt     : unsigned(26 downto 0);
+signal enable       : std_logic_vector(9 downto 0) := (others => '0');
+
+
+begin
+
+
+ps_start: process(clk_i)
+begin
+    if rising_edge(clk_i) then
+        
+        case sm_dac_start is
+        
+            -- Start by reseting everything 
+            when state_dac_start => 
+                wait_cnt <= (others => '0');
+                MODULE_ENABLE <= (others => '0'); 
+                DAC_CLKDIV <= (others => '0');
+                DAC_FIFO_RESET <= (others => '0');
+                DAC_FIFO_ENABLE <= (others => '0');
+                DAC_RESET <= (others => '0');
+                DAC_ENABLE  <= (others => '0'); 
+                enable <= enable(8 downto 0) & '1';
+                if enable(9) = '1' then
+                    sm_dac_start <= state_dac_module_enable;
+                end if;
+                
+            -- Enable the MODULE_ENABLE
+            when state_dac_module_enable => 
+                MODULE_ENABLE <= std_logic_vector(to_unsigned(1,32));
+                sm_dac_start <= state_dac_clkdiv;                  
+            
+            -- Target update rate of 1 MHz
+            -- Internal ACQ427 DAC clock = 62.5 MHz
+            -- Set DAC clock divide to 62, 62.5M / 62 ~= 1 MHz
+            when state_dac_clkdiv =>
+                DAC_CLKDIV <= std_logic_vector(to_unsigned(62,32));
+                sm_dac_start <= state_dac_reset_en; 
+            
+            -- Enable the DAC_RESET 
+            when state_dac_reset_en => 
+                DAC_RESET <= std_logic_vector(to_unsigned(1,32));
+                wait_cnt <= wait_cnt +1;
+                -- Allow the reset to be high for several clocks
+                if wait_cnt = c_dac_reset_wait then
+                    sm_dac_start <= state_dac_reset_dis;
+                end if;
+                
+            -- Disable the DAC_RESET    
+            when state_dac_reset_dis =>
+                wait_cnt <= (others => '0'); 
+                DAC_RESET <= (others => '0');
+                sm_dac_start <= state_dac_enable;     
+            
+            -- Enable the DAC_ENABLE
+            when state_dac_enable =>
+                wait_cnt <= wait_cnt +1;
+                -- Copied DTAC instruction but found i had to add in a delay before writting into the DAC_ENABLE register for a period of time
+                --fmc_dac_spi.vhd -- lines 189, 190, 191, 192 and 193 below shows that the DAC_ENABLE = '0' needs to be held 
+                --                -- off for a period of time before setting the DAC_ENABLE = '1' thats what the dac_en_wait_cnt 
+                --                -- is for  
+                --
+                --if  DAC_ENABLE = '0' and CONTROL_WRITE = '1' then		-- Start the Sequence when software writes to the register
+			    --    NEXT_CONTROL_STATE <= INIT_CNRL_WRITE;
+		        --elsif  DAC_ENABLE = '0' and CONTROL_READ = '1' then	-- Start the Sequence when software writes to the register
+			    --    NEXT_CONTROL_STATE <= INIT_CNRL_READ;
+		        --end if;                
+                if wait_cnt = c_dac_enable_wait then
+                    DAC_ENABLE <= std_logic_vector(to_unsigned(1,32));
+                    sm_dac_start <= state_dac_fifo_enable; 
+                end if;
+            
+            -- Enable the DAC_FIFO_ENABLE
+            when state_dac_fifo_enable =>
+                DAC_FIFO_ENABLE <= std_logic_vector(to_unsigned(1,32));
+            
+            -- Default condition
+            when others => 
+                sm_dac_start <= state_dac_start;
+           
+        end case;           
+    end if;            
+end process ps_start;    
+            
+            
+
+
+end rtl;           
