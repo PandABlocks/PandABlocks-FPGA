9ef7edf376463ca9ae29da2363bb958ac6121892
Michael Abbott <michael.abbott@diamond.ac.uk>
2018-12-11 14:03:35 +0000

    Fix ACQ427 GPIO bit assignment and bit field control

--- modules/fmc_adc427/extensions/fmc_adc427.py
+++ modules/fmc_adc427/extensions/fmc_adc427.py
@@ -3,36 +3,36 @@
 from i2c import smbus2
 
 # The mapping of GPIO bits is as follows:
-#   0.1:0   ADC A1 gain
-#   0.3:2   ADC A2 gain
-#   0.5:4   ADC A3 gain
-#   0.7:6   ADC A4 gain
-#   1.1:0   ADC B1 gain
-#   1.3:2   ADC B2 gain
-#   1.5:4   ADC B3 gain
-#   1.7:6   ADC B4 gain
+#   0.1:0   ADC A4 gain
+#   0.3:2   ADC A3 gain
+#   0.5:4   ADC A2 gain
+#   0.7:6   ADC A1 gain
+#   1.1:0   ADC B4 gain
+#   1.3:2   ADC B3 gain
+#   1.5:4   ADC B2 gain
+#   1.7:6   ADC B1 gain
 #   2.0     CLK_DIR (unused)
 #   2.1     TRIG_DIR (unused)
 #   2.2     DAC_RIBBON - 0 if DAC output fitted         (input)
 #   2.3     ADC_B_RIBBON - 0 if ADC B output fitted     (input)
-#   2.4     DAC 3 output range
-#   2.5     DAC 4 output range
-#   2.6     DAC 1 output range
-#   2.7     DAC 2 output range
+#   2.4     DAC 2 output range
+#   2.5     DAC 1 output range
+#   2.6     DAC 4 output range
+#   2.7     DAC 3 output range
 lookup_bit_map = {
     # All the write values are triple: (byte, offset, width)
-    'adc1_gain' : (0, 0, 2),
-    'adc2_gain' : (0, 2, 2),
-    'adc3_gain' : (0, 4, 2),
-    'adc4_gain' : (0, 6, 2),
-    'adc5_gain' : (1, 0, 2),
-    'adc6_gain' : (1, 2, 2),
-    'adc7_gain' : (1, 4, 2),
-    'adc8_gain' : (1, 6, 2),
-    'dac1_gain' : (2, 6, 1),
-    'dac2_gain' : (2, 7, 1),
-    'dac3_gain' : (2, 4, 1),
-    'dac4_gain' : (2, 5, 1),
+    'adc1_gain' : (0, 6, 2),
+    'adc2_gain' : (0, 4, 2),
+    'adc3_gain' : (0, 2, 2),
+    'adc4_gain' : (0, 0, 2),
+    'adc5_gain' : (1, 6, 2),
+    'adc6_gain' : (1, 4, 2),
+    'adc7_gain' : (1, 2, 2),
+    'adc8_gain' : (1, 0, 2),
+    'dac1_gain' : (2, 5, 1),
+    'dac2_gain' : (2, 4, 1),
+    'dac3_gain' : (2, 7, 1),
+    'dac4_gain' : (2, 6, 1),
     # The two read values are a pair: (byte, offset)
     'dac_ribbon' : (2, 2),
     'adc_ribbon' : (2, 3),
@@ -59,10 +59,9 @@ class GPIO_Helper:
         return bool(bits[byte_ix] & (1 << offset))
 
     def write_bits(self, (byte_ix, offset, width), value):
-        mask = (1 << width) - 1
-        value = value & mask
-        self.outputs[byte_ix] = \
-            (self.outputs[byte_ix] & ~mask) | (value << offset)
+        mask = ((1 << width) - 1) << offset
+        value = (value << offset) & mask
+        self.outputs[byte_ix] = (self.outputs[byte_ix] & ~mask) | value
         self.write_output_bits(self.outputs)
 
 class BitReader:
@@ -82,12 +81,16 @@ class BitsWriter:
         self.gpio.write_bits(self.offset, value)
 
 
+# We need a single GPIO controller shared between the ADC and DAC extensions.
+gpio_helper = GPIO_Helper()
+
+
 class Extension:
     def __init__(self, count):
-        self.gpio = GPIO_Helper()
+        pass
 
     def parse_read(self, request):
-        return BitReader(self.gpio, lookup_bit_map[request])
+        return BitReader(gpio_helper, lookup_bit_map[request])
 
     def parse_write(self, request):
-        return BitsWriter(self.gpio, lookup_bit_map[request])
+        return BitsWriter(gpio_helper, lookup_bit_map[request])
