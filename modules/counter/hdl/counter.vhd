ac83e45ca4103a965080b17362bb5cf3e47aff41
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-12-19 14:42:21 +0000

    Simplify counter logic

--- modules/counter/hdl/counter.vhd
+++ modules/counter/hdl/counter.vhd
@@ -48,7 +48,6 @@ constant c_min_val       : unsigned(31 downto 0) := x"80000000";
 
 constant c_step_size_one : std_logic_vector(31 downto 0) := x"00000001";
 
-signal step_enable      : std_logic;
 signal trigger_prev     : std_logic;
 signal trigger_rise     : std_logic;
 signal enable_prev      : std_logic;
@@ -81,18 +80,8 @@ enable_fall <= not enable_i and enable_prev;
 --------------------------------------------------------------------------
 -- Default counter STEP to 1
 --------------------------------------------------------------------------
-process(clk_i)
-begin
-    if rising_edge(clk_i) then
-        if STEP_WSTB = '1' then
-            step_enable <= '1';
-        elsif (enable_fall = '1') then
-            step_enable <= '0';
-        end if;
-    end if;
-end process;
 
-STEP_default <= STEP when step_enable = '1' else c_step_size_one;
+STEP_default <= c_step_size_one when unsigned(STEP) = to_unsigned(0,32) else STEP;
 
 --------------------------------------------------------------------------
 -- Up/Down Counter
@@ -100,6 +89,9 @@ STEP_default <= STEP when step_enable = '1' else c_step_size_one;
 -- on the rising edge of enable input.
 --------------------------------------------------------------------------
 process(clk_i)
+
+variable next_counter : signed(32 downto 0);
+
 begin
     if rising_edge(clk_i) then
         if ((MAX_WSTB = '1' or MIN_WSTB = '1') and unsigned(MAX) = to_unsigned(0,32) and unsigned(MIN) = to_unsigned(0,32)) then
@@ -112,50 +104,38 @@ begin
             MIN_VAL <= unsigned(MIN);
         end if;
 
-        -- Load the counter
-        if (START_WSTB = '1' and enable_i = '1') then
-            counter <= unsigned(START);
         -- Re-load on enable rising edge
-        elsif (enable_rise = '1') then
+        if (enable_rise = '1') then
             counter <= unsigned(START);
+        -- Drop the carry signal on falling enable
+        elsif (enable_fall = '1') then
+            counter_carry <= '0';
         -- Count up/down on trigger
         elsif (enable_i = '1' and trigger_rise = '1') then
+            -- Initialise next_counter with current value
+            next_counter := to_signed(0,33) + signed(counter);
             -- Direction
             if (dir_i = '0') then
-                -- Check to see if we are crossing from the positive to negative or
-                -- negative to positive boundaries if we do set the carry bit
-                if (signed(counter) + signed(STEP_default) > signed(MAX_VAL)) or
-                   (signed(counter) + signed(STEP_default) < signed(MIN_VAL)) or
-                   ((signed(counter) + signed(STEP_default) < signed(counter)))then
-                    counter_carry <= '1';
-                    -- Crossing boundary when going positive
-                    counter <= counter + (unsigned(STEP_default) - (MAX_VAL+1 - MIN_VAL));
-                else
-                    counter_carry <= '0';
-                    -- Increment the counter
-                    counter <= counter + unsigned(STEP_default);
-                end if;
+                next_counter := next_counter + signed(STEP_default);
             else
-                -- Check to see if we are crossing from the negative to positive or
-                -- positive to negative boundaries if we do set the carry bit
-                if (signed(counter) - signed(STEP_default) < signed(MIN_VAL)) or
-                   (signed(counter) - signed(STEP_default) > signed(MAX_VAL)) or
-                   ((signed(counter) - signed(STEP_default) > signed(counter)))then
-                    counter_carry <= '1';
-                    -- Crossing boundary when going negative
-                    counter <= counter - (unsigned(STEP_default) - (MAX_VAL+1 - MIN_VAL));
-                else
-                    counter_carry <= '0';
-                    -- Decrement the counter
-                    counter <= counter - unsigned(STEP_default);
-                end if;
+                next_counter := next_counter - signed(STEP_default);
             end if;
-        else
+            -- Check to see if we are crossing from the positive to negative or
+            -- negative to positive boundaries if we do set the carry bit
+            if (next_counter > signed(MAX_VAL)) then
+                -- Crossing boundary positive
+                counter_carry <= '1';
+                next_counter := next_counter - signed(MAX_VAL - MIN_VAL + 1);
+            elsif (next_counter < signed(MIN_VAL)) then
+                -- Crossing boundary negative
+                counter_carry <= '1';
+                next_counter := next_counter + signed(MAX_VAL - MIN_VAL + 1);
+            end if;
+            -- Increment the counter
+            counter <= unsigned(next_counter(31 downto 0));
+        elsif (trig_i = '0') then
             -- Need to stop the counter_carry when trig_i is low
-            -- this is what the python model does
-            if (trig_i = '0') then
-                counter_carry <= '0';
-            end if;
+            counter_carry <= '0';
         end if;
     end if;
 end process;
