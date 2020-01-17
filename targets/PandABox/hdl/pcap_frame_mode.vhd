f7907a7653601e9d7c3aa21ee2e6bf3a8f0af5d1
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-01-17 11:57:33 +0000

    Fix PCAP Difference mode if disarmed with gate high

--- targets/PandABox/hdl/pcap_frame_mode.vhd
+++ targets/PandABox/hdl/pcap_frame_mode.vhd
@@ -79,12 +79,7 @@ begin
  ps_diff: process(clk_i)
  begin
     if rising_edge(clk_i) then
-        gate_prev <= gate_i;
-        -- Latch the current value to start_val on rising gate or on capture
-        if (gate_prev = '0' and gate_i = '1') or (trig_i = '1' and gate_i = '1') then
-            start_val <= signed(value_i);
-        end if;
-        -- Zero diff_sum and set diff output on capture
+        -- Output the result
         if (trig_i = '1') then
             if (gate_i = '1' or gate_prev = '1') then
                 -- Diff output is current diff + diff_sum if capture and (falling or high gate)
@@ -92,11 +87,19 @@ begin
             else
                 diff_o <= std_logic_vector(diff_sum);
             end if;
+        end if;
+        -- Clear diff on disable or trigger with no gate high
+        if (enable_i = '0') or (trig_i = '1') then
             diff_sum <= (others => '0');
         -- Add in current diff to diff_sum if falling gate
         elsif (gate_prev = '1' and gate_i = '0') then
             diff_sum <= diff_sum + signed(value_i) - start_val;
         end if;
+        -- Latch the current value to start_val on rising gate or on capture
+        if (gate_prev = '0' and gate_i = '1') or (trig_i = '1' and gate_i = '1') then
+            start_val <= signed(value_i);
+        end if;
+        gate_prev <= gate_i;
     end if;
  end process ps_diff;
 
@@ -113,11 +116,10 @@ begin
             -- Mode 3 Sum High data
             sum_h_o <= std_logic_vector(sum_data(c_thirty_one+c_thirty_two+(to_integer(unsigned(shift_i))) downto c_thirty_two+(to_integer(unsigned(shift_i)))));
         end if;
-        -- Clear sum
-        if (enable_i = '0') then
+        -- Clear sum on disable or trigger with no gate high
+        if (enable_i = '0') or (trig_i = '1' and gate_i = '0') then
             sum_data <= (others => '0');
-        elsif (trig_i = '1' and gate_i = '0') then
-            sum_data <= (others => '0');
+        -- Set sum to current value on trigger with gate high
         elsif (trig_i = '1' and gate_i = '1') then
             sum_data <= resize(signed(value_i),sum_data'length);
         -- Sum the received data whilst gate high
@@ -134,24 +136,19 @@ begin
  ps_min_max: process(clk_i)
  begin
     if rising_edge(clk_i) then
-        -- Outpput th result
+        -- Output the result
         if trig_i = '1' then
             min_o <= std_logic_vector(min_val);
             max_o <= std_logic_vector(max_val);
         end if;
-        -- Clear min and max values
-        if (enable_i = '0') then
-            -- Maximum value
+        -- Clear min and max values on disable or trigger with no gate high
+        if (enable_i = '0') or (trig_i = '1' and gate_i = '0') then
             min_val <= (min_val'high => '0', others => '1');
-            -- Minimum value
             max_val <= (max_val'high => '1', others => '0');
-        -- At the start capture the first value
+        -- Store current value on trigger with gate high
         elsif (trig_i = '1' and gate_i = '1') then
             min_val <= signed(value_i);
             max_val <= signed(value_i);
-        elsif (trig_i = '1' and gate_i = '0') then
-            min_val <= (min_val'high => '0', others => '1');
-            max_val <= (max_val'high => '1', others => '0');
         elsif (gate_i = '1') then
             -- Capture the minimum number
             if signed(value_i) < min_val then
