b43546b7a4556d6a003bc8a2bf0d2fb1064dfdac
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-09 13:53:47 +0100

    Change cat statement to a quote rather than allowing substitutions

--- .github/scripts/mount-s3-bucket.sh
+++ .github/scripts/mount-s3-bucket.sh
@@ -5,18 +5,16 @@ mkdir -p /home/runner/.config/rclone
 cd /home/runner/.config/rclone/
 touch rclone.conf
 
-cat >> rclone.conf <<EOL
+cat >> rclone.conf <<'EOL'
 [fpga-vivado]
 type = s3
 provider = Ceph
 env_auth = false
-access_key_id = /${{ secrets.VIVADO_S3_ACCESS_KEY_ID }}
-secret_access_key = /${{ secrets.VIVADO_S3_SECRET_ACCESS_KEYD }}
+access_key_id = ${{ secrets.VIVADO_S3_ACCESS_KEY_ID }}
+secret_access_key = ${{ secrets.VIVADO_S3_SECRET_ACCESS_KEYD }}
 region =
 endpoint = https://s3.echo.stfc.ac.uk
 EOL
-
-cat rclone.conf
 
 sudo mkdir -p /tools/Xilinx
 rclone mount --file-perms 0777 --attr-timeout=10m --no-modtime --read-only --daemon fpga-vivado:dls-controls-fpga-vivado /tools/Xilinx
