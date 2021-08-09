1eafde75211d8e27d32f7014219e9a9fb2acbca3
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-09 11:56:31 +0100

    Move s3 bucket job to a bash script

---
+++ .github/scripts/mount-s3-bucket.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Grants access to the s3 bucket containing vivado
+
+cat >> ${HOME}/.config/rclone/rclone.conf <<EOL
+[fpga-vivado]
+type = s3
+provider = Ceph
+env_auth = false
+access_key_id = ${{ secrets.VIVADO_S3_ACCESS_KEY_ID }}
+secret_access_key = ${{ secrets.VIVADO_S3_SECRET_ACCESS_KEYD }}
+region =
+endpoint = https://s3.echo.stfc.ac.uk
+EOL
+
+sudo mkdir -p /tools/Xilinx
+rclone mount --file-perms 0777 --attr-timeout=10m --no-modtime --read-only --daemon fpga-vivado:dls-controls-fpga-vivado /tools/Xilinx
+source /tools/Xilinx/Vivado/2020.2/settings64.sh
+vivado
+fusermount -u /tools/Xilinx
