4d7ca8fd08d774952536801c5d6ed9953a71dad4
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-10 10:49:12 +0100

    Skip copy step

--- .github/scripts/mount-s3-bucket.sh
+++ .github/scripts/mount-s3-bucket.sh
@@ -4,8 +4,7 @@
 S3_ACCESS_KEY_ID=$1
 S3_SECRET_ACCESS_KEY=$2
 
-sudo mkdir -p /tools/Xilinx/Vitis_HLS
-sudo mkdir -p /tools/Xilinx/Vivado
+sudo mkdir -p /tools/Xilinx
 mkdir -p /home/runner/.config/rclone
 cd /home/runner/.config/rclone/
 touch rclone.conf
@@ -21,11 +20,9 @@ region =
 endpoint = https://s3.echo.stfc.ac.uk
 EOL
 
-cat rclone.conf
-
-chmod 600 rclone.conf
-rclone copy -P -l /tools/Xilinx/Vitis_HLS fpga-vivado:dls-controls-fpga-vivado/Vitis_HLS
-rclone copy -P -l /tools/Xilinx/Vivado fpga-vivado:dls-controls-fpga-vivado/Vivado
+#chmod 600 rclone.conf
+#rclone copy -P -l /tools/Xilinx/Vitis_HLS fpga-vivado:dls-controls-fpga-vivado/Vitis_HLS
+#rclone copy -P -l /tools/Xilinx/Vivado fpga-vivado:dls-controls-fpga-vivado/Vivado
 
 sudo rclone mount --file-perms 0777 --attr-timeout=10m --no-modtime --read-only --daemon fpga-vivado:dls-controls-fpga-vivado /tools/Xilinx
 source /tools/Xilinx/Vivado/2020.2/settings64.sh
