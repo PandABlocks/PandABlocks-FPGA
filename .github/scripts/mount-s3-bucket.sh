c522077dd4062c0f8b46fee3b5e451a633cd9d63
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-31 15:43:24 +0100

    Revert rclone config to specific runner dir

--- .github/scripts/mount-s3-bucket.sh
+++ .github/scripts/mount-s3-bucket.sh
@@ -6,9 +6,9 @@
 
 sudo mkdir -p /scratch/Xilinx
 sudo chmod a+w /scratch
-mkdir -p ~/.config/rclone
+mkdir -p /home/runner/.config/rclone
 
-cat >> ~/.config/rclone/rclone.conf <<EOL
+cat >> /home/runner/.config/rclone/rclone.conf <<EOL
 [fpga-vivado]
 type = s3
 provider = Ceph
@@ -19,5 +19,5 @@ region =
 endpoint = https://s3.echo.stfc.ac.uk
 EOL
 
-chmod 600 ~/.config/rclone/rclone.conf
+chmod 600 /home/runner/.config/rclone/rclone.conf
 rclone mount --file-perms 0777 --attr-timeout=10m --no-modtime --read-only --daemon --allow-other --vfs-cache-mode full fpga-vivado:dls-controls-fpga-xilinx /scratch/Xilinx
