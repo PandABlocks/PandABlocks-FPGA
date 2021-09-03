c4b2b9d93ecf474316136d10066a3387de4982c8
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-09-03 16:41:37 +0100

    Update make_impi_ini to python 3

--- common/python/make_ipmi_ini.py
+++ common/python/make_ipmi_ini.py
@@ -41,7 +41,8 @@ def main():
 
     if not ini_found:
         # If we failed to find a file generate fallback instead
-        file(target, 'w').write(fallback_ini)
+        f = open(target, 'w').write(fallback_ini)
+        f.close()
 
 if __name__ == '__main__':
     main()
