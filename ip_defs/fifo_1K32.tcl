ba9eea4eb2e1108345116e9ba03f9c37356c4d0b
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-11-04 14:52:54 +0000

    Add missing parameters in IP definitions
    In Zynq UltraScale+, some parameters are mandatory or have conflicting
    defaults.
    The default fifo implentation is common clock native, which does
    not support having a data count output.
    Another difference is that the 'asymetric port width' flag is needed when
    the input data width and the output data width don't match.

--- ip_defs/fifo_1K32.tcl
+++ ip_defs/fifo_1K32.tcl
@@ -5,6 +5,7 @@ create_ip -vlnv [get_ipdefs -filter {NAME == fifo_generator}] \
 -module_name fifo_1K32 -dir $BUILD_DIR/
 
 set_property -dict [list \
+    CONFIG.Fifo_Implementation {Common_Clock_Distributed_RAM} \
     CONFIG.Input_Data_Width {32}    \
     CONFIG.Data_Count {true}        \
     CONFIG.Output_Data_Width {32}   \
