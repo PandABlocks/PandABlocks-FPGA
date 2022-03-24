85501680f53ca943e68920f9f4542f61a5de82d8
glennchid <glennchid@users.noreply.github.com>
2022-03-24 15:28:23 +0000

    deploy: 7ec8194b73ef67310bbea4904e54ce24c53d5aeb

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0a2-18-g109cfa6',
+    VERSION: '3.0a2-21-g7ec8194',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
