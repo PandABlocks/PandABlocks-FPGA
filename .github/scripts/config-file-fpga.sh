e4d5a426ce9e927344a0c01994b68b5906e4a2fc
Tom Trafford <tom.trafford@diamond.ac.uk>
2022-07-20 15:16:45 +0100

    Generate CONFIG within CI keeping original role of CONFIG.example

--- .github/scripts/config-file-fpga.sh
+++ .github/scripts/config-file-fpga.sh
@@ -3,7 +3,7 @@
 
 cat >> PandABlocks-fpga/CONFIG << 'EOL'
 # Default build location.  Default is to build in build subdirectory.
-BUILD_DIR = $(GITHUB_WORKSPACE)/build
+BUILD_DIR = /build/PandA-FPGA
 
 # Development Tool Version
 export VIVADO_VER = 2020.2
@@ -13,13 +13,13 @@ export VIVADO = /scratch/Xilinx/Vivado/\$(VIVADO_VER)/settings64.sh
 
 # Location of rootfs builder.  This needs to be at least version 1.13 and can be
 # downloaded from https://github.com/araneidae/rootfs
-export ROOTFS_TOP = $(GITHUB_WORKSPACE)/rootfs
+export ROOTFS_TOP = /rootfs
 
 # Where to find source files
-export TAR_FILES = $(GITHUB_WORKSPACE)/tar-files
+export TAR_FILES = /tar-files
 
 # Path to root filesystem
-PANDA_ROOTFS = $(GITHUB_WORKSPACE)/PandABlocks-rootfs
+PANDA_ROOTFS = /repos/PandABlocks-rootfs
 MAKE_ZPKG = $(PANDA_ROOTFS)/make-zpkg
 
 # Python interpreter for running scripts
