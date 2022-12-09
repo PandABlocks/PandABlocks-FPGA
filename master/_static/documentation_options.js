d7db11c4d796c0bc445fce46f7905f57ae91aebd
tomtrafford <tomtrafford@users.noreply.github.com>
2022-12-09 14:30:56 +0000

    deploy: d1a157f4ea55b3a6544b9270539ecbb549ac2444

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,10 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0a2-77-g87b0c9a',
+    VERSION: '3.0a2-80-gd1a157f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: false
 };
