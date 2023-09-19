fda9532ef756bc4802428054205f1c4af74de370
Tom Trafford <tom.trafford@diamond.ac.uk>
2023-09-19 14:45:16 +0100

    Refactored fmc_acq427.py and fixed dac_gain register interaction

--- modules/fmc_acq427/extensions/fmc_acq427.py
+++ modules/fmc_acq427/extensions/fmc_acq427.py
@@ -18,20 +18,23 @@ from i2c import smbus2
 #   2.5     DAC 1 output range
 #   2.6     DAC 4 output range
 #   2.7     DAC 3 output range
-lookup_bit_map = {
+lookup_write_bit_map = {
     # All the write values are triple: (byte, offset, width)
-    'adc1_gain' : (0, 6, 2),
-    'adc2_gain' : (0, 4, 2),
-    'adc3_gain' : (0, 2, 2),
-    'adc4_gain' : (0, 0, 2),
-    'adc5_gain' : (1, 6, 2),
-    'adc6_gain' : (1, 4, 2),
-    'adc7_gain' : (1, 2, 2),
-    'adc8_gain' : (1, 0, 2),
-    'dac1_gain' : (2, 5, 1),
-    'dac2_gain' : (2, 4, 1),
-    'dac3_gain' : (2, 7, 1),
-    'dac4_gain' : (2, 6, 1),
+    'adc1_gain' : ("write_adc_gain", (0, 6, 2)),
+    'adc2_gain' : ("write_adc_gain", (0, 4, 2)),
+    'adc3_gain' : ("write_adc_gain", (0, 2, 2)),
+    'adc4_gain' : ("write_adc_gain", (0, 0, 2)),
+    'adc5_gain' : ("write_adc_gain", (1, 6, 2)),
+    'adc6_gain' : ("write_adc_gain", (1, 4, 2)),
+    'adc7_gain' : ("write_adc_gain", (1, 2, 2)),
+    'adc8_gain' : ("write_adc_gain", (1, 0, 2)),
+    'dac1_gain' : ("write_dac_gain", (2, 5, 1)),
+    'dac2_gain' : ("write_dac_gain", (2, 4, 1)),
+    'dac3_gain' : ("write_dac_gain", (2, 7, 1)),
+    'dac4_gain' : ("write_dac_gain", (2, 6, 1)),
+}
+
+lookup_read_bit_map = {
     # The two read values are a pair: (byte, offset)
     'dac_ribbon' : (2, 2),
     'adc_ribbon' : (2, 3),
@@ -62,7 +65,14 @@ class GPIO_Helper:
         shift_value = (value << offset) & mask
         self.outputs[byte_ix] = (self.outputs[byte_ix] & ~mask) | shift_value
         self.write_output_bits(self.outputs)
+
+    def write_adc_gain(self, value, *args):
+        self.write_bits(value, *args)
         return (value,)
+
+    def write_dac_gain(self, *args):
+        # dac_gains do not write to a register and should not return a value
+        self.write_bits(*args)
 
 
 # We need a single GPIO controller shared between the ADC and DAC extensions.
@@ -74,9 +84,10 @@ class Extension:
         pass
 
     def parse_read(self, request):
-        offset = lookup_bit_map[request]
-        return lambda _: gpio_helper.read_bit(*offset)
+        offsets = lookup_read_bit_map[request]
+        return lambda _: gpio_helper.read_bit(*offsets)
 
     def parse_write(self, request):
-        offset = lookup_bit_map[request]
-        return lambda _, value: gpio_helper.write_bits(value, *offset)
+        action, offsets = lookup_write_bit_map[request]
+        action = getattr(gpio_helper, action)
+        return lambda _, value: action(value, *offsets)
