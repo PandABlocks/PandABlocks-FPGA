1910bc46c32ff567501ff812c951f6860d22ddf2
glennchid <glennchid@users.noreply.github.com>
2024-01-15 10:22:06 +0000

    deploy: 23e97c8080545761df7512992fce447799f778f3

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0b1-19-gd28645e',
+    VERSION: '3.0-1-g23e97c8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
