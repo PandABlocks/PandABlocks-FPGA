450181feb38ce33324c0f126d1428c0e44754076
Shihab Suliman <rye74444@pw064.diamond.ac.uk>
2024-04-04 14:14:03 +0100

    Removed Sudo command from go install

--- .github/scripts/rclone-patch.sh
+++ .github/scripts/rclone-patch.sh
@@ -8,7 +8,7 @@ fi
 
 #Install go
 curl -OL https://golang.org/dl/go1.17.linux-amd64.tar.gz
-rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
