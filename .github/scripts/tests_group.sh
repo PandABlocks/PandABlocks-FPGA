dce286535d4d1ce62db2621c4c4d79d2b552c0a9
Shihab Suliman <rye74444@localhost.localdomain>
2024-05-04 16:53:03 +0100

    new test jobs

--- .github/scripts/tests_group.sh
+++ .github/scripts/tests_group.sh
@@ -4,8 +4,20 @@
 search_dir=$1
 work_dir=$2
 
-# Find files matching timing name pattern
-found_files=$(find "$search_dir" -type f -name "*.timing.ini")
+# Find files matching timing name pattern (Exclude specific files such as seq_double_table)
+# This executes a bash script on the found files, wherein they are only included if the timing test file matches the parent directory name (i.e., module name)
+# This includes files like seq.timing.ini but excludes seq_double_table.timing.ini
+# This is important to allow for module names to be passed to make hdl_test argument
+# When splitting off functionality and documentation timing.ini tests, this needs to be checked again
+found_files=$(find "$search_dir" -type f -name "*.timing.ini" -exec bash -c '
+    for file; do
+        module_name=$(basename "$(dirname "$file")")
+        module_timing_name=$(basename "$file")
+        if [[ "$module_timing_name" =~ ^"$module_name".timing.ini$ ]]; then
+            echo "$file"
+        fi
+    done
+' bash {} +)
 
 # Array of modules and their test count
 module_grps=()
