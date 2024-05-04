1378a789318f90eb8e4f5527944fead912b57d70
Shihab Suliman <rye74444@localhost.localdomain>
2024-05-04 15:20:47 +0100

    new test jobs

---
+++ .github/scripts/round_robin.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+modules_grps=(sys.argv)[1:]
+modules=[]
+num_jobs=5
+
+def split_modules(modules,num_jobs):
+    modules.sort(key=lambda x: x[1], reverse=True)
+    jobs = [[] for _ in range(num_jobs)]
+    for module, count in modules:
+        min_sum_job_idx = min(range(num_jobs), key=lambda i: sum(subset[1] for subset in jobs[i]))
+        jobs[min_sum_job_idx].append([module,count])
+    return jobs
+
+def generate_matrix(jobs):
+    matrix = {'include':{'modules':[]}}
+    for job in jobs:
+        job_include = ' '.join(module[0] for module in job)
+        matrix['include']['modules'].append(job_include)
+    return matrix
+
+for i in range(0,len(modules_grps)-1,2):
+    modules.append([modules_grps[i],int(modules_grps[i+1])])
+
+with open("github_tests.json", "w") as matrix_file:
+    json.dump(generate_matrix(split_modules(modules,num_jobs)), matrix_file, indent=4)
---
+++ .github/scripts/tests_group.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Modules path
+search_dir=$1
+
+# Find files matching timing name pattern
+found_files=$(find "$search_dir" -type f -name "*.timing.ini")
+
+# Array of modules and their test count
+module_grps=()
+
+# Check if any files were found
+if [ -z "$found_files" ]; then
+    echo "No timing tests found"
+else
+    # Loop through each found file
+    for file in $found_files; do
+        # Count occurrences of [*] in the file
+        # Min 5 char to filter descriptions and index
+        count=$(grep -o '\[[^][]\{5,\}\]' "$file" | wc -l)
+        module_name=$(basename "$file" .timing.ini)
+        module_grps+=("$module_name" "$count")
+    done
+    # Run python script to define job matrix based on found tests
+    python3 round_robin.py "${module_grps[@]}"
+
+fi
+
