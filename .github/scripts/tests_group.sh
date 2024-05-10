ba418721c6550d933f12bbde1a5f05de11799c01
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2024-05-10 11:11:50 +0100

    Trying to use dirname in array

--- .github/scripts/tests_group.sh
+++ .github/scripts/tests_group.sh
@@ -5,19 +5,7 @@ search_dir=$1
 work_dir=$2
 
 # Find files matching timing name pattern
-# This executes a bash script on the found files, wherein they are only included if the timing test file matches the parent directory name (i.e., module name)
-# This includes files like seq.timing.ini but excludes seq_double_table.timing.ini
-# This is important to allow for module names to be passed to make hdl_test argument
-# When splitting off functionality and documentation timing.ini tests, this needs to be checked again
-found_files=$(find "$search_dir" -type f -name "*.timing.ini" -exec bash -c '
-    for file; do
-        module_name=$(basename "$(dirname "$file")")
-        module_timing_name=$(basename "$file")
-        if [[ "$module_timing_name" =~ ^"$module_name".timing.ini$ ]]; then
-            echo "$file"
-        fi
-    done
-' bash {} +)
+found_files=$(find "$search_dir" -type f -name "*.timing.ini")
 
 # Array of modules and their test count
 module_grps=()
@@ -31,7 +19,7 @@ else
         # Count occurrences of [*] in the file
         # Min 5 char to filter descriptions and index
         count=$(grep -o '\[[^][]\{5,\}\]' "$file" | wc -l)
-        module_name=$(basename "$file" .timing.ini)
+        module_name=$(basename "$(dirname "$file")")
         module_grps+=("$module_name" "$count")
     done
     # Run python script to define job matrix based on found tests
