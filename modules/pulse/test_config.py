0b5aca08f287e4caad0db4804d5c3f77f6bdcd35
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2024-09-20 08:49:52 +0100

    Add cocotb test runner

---
+++ modules/pulse/test_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+EXTRA_HDL_FILES = [TOP_PATH / 'common' / 'hdl' / 'defines' / 'support.vhd',
+                   TOP_PATH / 'common' / 'hdl' / 'defines' / 'top_defines.vhd',
+                   BUILD_PATH / 'apps' / 'PandABox-fmc_lback-sfp_lback' / 'autogen' / 'hdl' / 'top_defines_gen.vhd',
+                   TOP_PATH / 'common' / 'hdl' / 'fifo.vhd']
