37096ad5058fbe0e926a987f9964994e9badd9f2
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2024-10-10 09:07:32 +0100

    Test config files for seq and pcomp modules

---
+++ modules/seq/test_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+EXTRA_HDL_FILES = [TOP_PATH / 'common' / 'hdl' / 'defines' / 'support.vhd',
+                   TOP_PATH / 'common' / 'hdl' / 'defines' / 'top_defines.vhd',
+                   TOP_PATH / 'common' / 'hdl' / 'spbram.vhd',
+                   TOP_PATH / 'build' / 'apps' / 'PandABox-no-fmc' / 'autogen' / 'hdl' / 'top_defines_gen.vhd']
+
+EXTRA_BUILD_ARGS = ['-frelaxed']
