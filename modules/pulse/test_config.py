035372acb84ef892bc6daed6a4df72be9989ce61
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2024-10-23 15:41:35 +0100

    Changing test config files to reflect changes in support.vhd. top_defines.vhd now needed

--- modules/pulse/test_config.py
+++ modules/pulse/test_config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 EXTRA_HDL_FILES = [TOP_PATH / 'common' / 'hdl' / 'defines' / 'support.vhd',
+                   TOP_PATH / 'common' / 'hdl' / 'defines' / 'top_defines.vhd',
+                   TOP_PATH / 'build' / 'apps' / 'PandABox-no-fmc' / 'autogen' / 'hdl' / 'top_defines_gen.vhd',
                    TOP_PATH / 'common' / 'hdl' / 'fifo.vhd']
 
 EXTRA_BUILD_ARGS = ['-Wno-hide']
