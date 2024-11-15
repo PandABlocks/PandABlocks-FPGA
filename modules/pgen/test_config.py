122788524b342cdda4df014a586d1ee43b31ded0
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2024-11-15 16:06:32 +0000

    Changing so that all modules use PandABox-fmc_lback-sfp_lback build

--- modules/pgen/test_config.py
+++ modules/pgen/test_config.py
@@ -2,7 +2,5 @@
 
 EXTRA_HDL_FILES = [TOP_PATH / 'common' / 'hdl' / 'defines' / 'support.vhd',
                    TOP_PATH / 'common' / 'hdl' / 'defines' / 'top_defines.vhd',
-                   TOP_PATH / 'build' / 'apps' / 'PandABox-no-fmc' / 'autogen' / 'hdl' / 'top_defines_gen.vhd',
+                   TOP_PATH / 'build' / 'apps' / 'PandABox-fmc_lback-sfp_lback' / 'autogen' / 'hdl' / 'top_defines_gen.vhd',
                    TOP_PATH / 'common' / 'hdl' / 'fifo.vhd']
-
-# EXTRA_BUILD_ARGS = ['-Wno-hide']
--- modules/seq/test_config.py
+++ modules/seq/test_config.py
@@ -3,8 +3,4 @@
 EXTRA_HDL_FILES = [TOP_PATH / 'common' / 'hdl' / 'defines' / 'support.vhd',
                    TOP_PATH / 'common' / 'hdl' / 'defines' / 'top_defines.vhd',
                    TOP_PATH / 'common' / 'hdl' / 'spbram.vhd',
-                   TOP_PATH / 'build' / 'apps' / 'PandABox-no-fmc' / 'autogen' / 'hdl' / 'top_defines_gen.vhd']
-
-# EXTRA_BUILD_ARGS = ['-frelaxed']
-# Use -frelaxed when simulating shared variables. Turns an error into a warning.
-
+                   TOP_PATH / 'build' / 'apps' / 'PandABox-fmc_lback-sfp_lback' / 'autogen' / 'hdl' / 'top_defines_gen.vhd']
