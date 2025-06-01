e1caa36b50f4f4572a2e8bcd12760acce980ab5c
Emilio Perez <emiliopeju@gmail.com>
2025-06-01 14:28:49 +0100

    seq: remove development tests

--- modules/seq/sim/common.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def get_top():
-    current = Path(__file__).resolve()
-    while not (current / '.git').exists():
-        current = current.parent
-
-    return current
