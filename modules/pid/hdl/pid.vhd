b86767ca242d1cd88d2121a265c1e6a33ce70411
Richard Cunningham <richard.cunningham@diamond.ac.uk>
2025-06-03 16:41:34 +0100

    Rework to pass timing consistraints

--- modules/pid/hdl/pid.vhd
+++ modules/pid/hdl/pid.vhd
@@ -10,85 +10,107 @@ use ieee.numeric_std.all;
 
 entity PID is 
 port (
-    clk_i             : in  std_logic; -- Forced by PandA
-    init_i            : in  std_logic;
-    slo_clk_i         : in  std_logic;
+    clk_i                : in  std_logic; -- Forced by PandA
+    init_i               : in  std_logic;
+    trig_i               : in  std_logic;
 
     -- Pass in constants externally
-    Kp_i              : in  std_logic_vector(31 downto 0);
-    Ki_i              : in  std_logic_vector(31 downto 0);
-    Kd_i              : in  std_logic_vector(31 downto 0);
-    FF_i              : in  std_logic_vector(31 downto 0);
-     
-    setpoint_i        : in  std_logic_vector(31 downto 0); -- Desired value
-    in_signal_i       : in  std_logic_vector(31 downto 0); -- Measured value
-    out_signal_o      : out std_logic_vector(31 downto 0)  -- Output value
+    Kp_i                 : in  std_logic_vector(31 downto 0);
+    Ki_i                 : in  std_logic_vector(31 downto 0);
+    Kd_i                 : in  std_logic_vector(31 downto 0);
+    FF_i                 : in  std_logic_vector(31 downto 0);
+
+    setpoint_i           : in  std_logic_vector(31 downto 0); -- Desired value
+    in_signal_i          : in  std_logic_vector(31 downto 0); -- Measured value
+    out_signal_o         : out std_logic_vector(31 downto 0)  -- Output value
 );
 end entity PID;
 
 architecture Basic_PID of PID is
-    signal in_error   : signed(31 downto 0) := (others => '0');
-    signal prev_error : signed(31 downto 0) := (others => '0'); -- Start from zero
-    signal integral   : signed(63 downto 0) := (others => '0'); -- Start from zero
-    signal derivative : signed(31 downto 0) := (others => '0');
-    signal P_out      : signed(63 downto 0) := (others => '0');
-    signal I_out      : signed(63 downto 0) := (others => '0');
-    signal D_out      : signed(63 downto 0) := (others => '0');
-    signal FF_out     : signed(63 downto 0) := (others => '0');
-    signal round_out  : signed(63 downto 0) := (others => '0');
+    signal setpoint      : signed(25 downto 0) := (others => '0');
+    signal in_error      : signed(25 downto 0) := (others => '0');
+    signal prev_error    : signed(25 downto 0) := (others => '0');
 
-    -- For type conversion
-    signal Kp         : signed(31 downto 0) := (others => '0');
-    signal Ki         : signed(31 downto 0) := (others => '0');
-    signal Kd         : signed(31 downto 0) := (others => '0');
-    signal FF         : signed(31 downto 0) := (others => '0'); -- Feed Forward
-    signal setpoint   : signed(31 downto 0) := (others => '0');
-    signal in_signal  : signed(31 downto 0) := (others => '0');
-    signal out_signal : signed(31 downto 0) := (others => '0');
+    signal P_out         : signed(44 downto 0) := (others => '0');
+    signal I_out         : signed(44 downto 0) := (others => '0');
+    signal D_out         : signed(44 downto 0) := (others => '0');
+    signal FF_out        : signed(44 downto 0) := (others => '0');
+
+    signal Kp            : signed(18 downto 0) := (others => '0');
+    signal Ki            : signed(18 downto 0) := (others => '0');
+    signal Kd            : signed(18 downto 0) := (others => '0');
+    signal FF            : signed(18 downto 0) := (others => '0');
+
+    signal in_signal     : signed(25 downto 0) := (others => '0');
+    signal round_out     : signed(44 downto 0) := (others => '0');
+    signal out_signal    : signed(31 downto 0) := (others => '0');
+
+    signal trigger       : std_logic;
+    signal trigger_prev  : std_logic;
+
+    signal I_out_current : signed(44 downto 0) := (others => '0');
 
 begin
 
-    -- Conversions forced by FPGA weirdness
-    Kp            <= signed(Kp_i);
-    Ki            <= signed(Ki_i);
-    Kd            <= signed(Kd_i);
-    FF            <= signed(FF_i);
-    setpoint      <= signed(setpoint_i);
-    in_signal     <= signed(in_signal_i);
+    process(clk_i)
+        constant max_lim_final : signed(out_signal'range) := (out_signal'left => '0', others => '1');
+        constant min_lim_final : signed(out_signal'range) := (out_signal'left => '1', others => '0');
 
-    process(slo_clk_i)
-        constant max_lim : signed(out_signal'range) := (out_signal'left => '0', others => '1');
-        constant min_lim : signed(out_signal'range) := (out_signal'left => '1', others => '0');
+        constant max_lim_err  : signed(out_signal'range) := (out_signal'left => '0', others => '1');
+        constant min_lim_err  : signed(out_signal'range) := (out_signal'left => '1', others => '0');
 
     begin
-        if rising_edge(slo_clk_i) then
+        if rising_edge(clk_i) then
+            -- PandA type conversions
+            Kp             <= resize(signed(Kp_i), 19);
+            Ki             <= resize(signed(Ki_i), 19);
+            Kd             <= resize(signed(Kd_i), 19);
+            FF             <= resize(signed(FF_i), 19);
+            setpoint       <= resize(signed(setpoint_i), 26);
+            in_signal      <= resize(signed(in_signal_i), 26);
+
+            trigger_prev   <= trig_i;
+
+            I_out <= I_out_current;
+
+            -- Error calc w/ overflow protection
+            if setpoint - in_signal > max_lim_err then
+                report "OVERFLOW HIGH";
+                in_error  <= (in_error'left => '0', others => '1');
+            elsif setpoint - in_signal < min_lim_err then
+                in_error  <= (in_error'left => '1', others => '0');
+                report "OVERFLOW LOW";
+            else
+                in_error     <= resize(setpoint - in_signal, 26);
+            end if;
+
             if init_i = '1' then
                 round_out  <= (others => '0');
                 P_out      <= (others => '0');
                 I_out      <= (others => '0');
                 D_out      <= (others => '0');
                 FF         <= (others => '0');
-            else
+            elsif trigger = '1' then
                 -- Proportional
-                P_out      <= (Kp * in_error);
+                P_out    <= (Kp * in_error);
 
                 -- Integral
-                I_out      <= I_out + (Ki * in_error);
+                I_out_current <= (Ki * in_error);
 
                 -- Derivative
-                D_out      <= Kd * (in_error - prev_error);
+                D_out    <= Kd * (in_error - prev_error);
 
                 -- Feedforwards
-                FF_out     <= FF * setpoint;
+                FF_out     <= (FF * setpoint);
 
                 -- Update
                 prev_error <= in_error;
 
                 -- Protect against overflow
-                if P_out + I_out + D_out + FF_out > max_lim then
+                if P_out + I_out + D_out + FF_out > max_lim_final then
                     report "OVERFLOW HIGH";
                     round_out  <= (round_out'left => '0', others => '1');
-                elsif P_out + I_out + D_out + FF_out < min_lim then
+                elsif P_out + I_out + D_out + FF_out < min_lim_final then
                     round_out  <= (round_out'left => '1', others => '0');
                     report "OVERFLOW LOW";
                 else
@@ -99,8 +121,10 @@ begin
         end if; -- Clock
     end process;
 
-    -- Error calc
-    in_error     <= setpoint - in_signal;
+    -- trigger if clock is high and previous was not high
+    -- only triggers for one clock period on rising edge
+    trigger      <= trig_i and not trigger_prev;
+    
 
     -- Output conversions
     out_signal   <= resize(round_out, 32);
