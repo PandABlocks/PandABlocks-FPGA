11e32c472cef888de76cb8d6ed3a40ef873191f6
Emilio Perez Juarez <emiliopeju@gmail.com>
2025-10-23 10:10:47 +0100

    Rename targets to match the same rules as app naming
    This is for consistency and because in some context, the name of the
    target is assumed to be the first part of the app name.

--- targets/xu5/target_incl.tcl
+++ targets/xu5/target_incl.tcl
@@ -1,9 +1,10 @@
-set FPGA_PART xc7z020clg484-1
-set HDL_TOP ZedBoard_top
+set FPGA_PART xczu4ev-sfvc784-1-i
+set HDL_TOP xu5_top
 
 # Target specific Constriants to be read
 # NB: we could just read the entire directory with 'add_files [glob $TARGET_DIR/const/*.xdc]
 set CONSTRAINTS { \
-            ZedBoard-pins_impl.xdc
+            xu5-pins_impl.xdc \
+            xu5-clks.xdc
 }
 
