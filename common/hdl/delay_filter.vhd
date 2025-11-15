23b9ec862afbec79f4f5ff2e4494bdb035e5474c
Emilio Perez Juarez <emiliopeju@gmail.com>
2025-11-15 21:20:19 +0000

    Remove useless unisim imports that make simulation harder/slower

--- common/hdl/delay_filter.vhd
+++ common/hdl/delay_filter.vhd
@@ -17,9 +17,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-library unisim;
-use unisim.vcomponents.all;
-
 entity delay_filter is
 port (
     -- Clock and reset signals
