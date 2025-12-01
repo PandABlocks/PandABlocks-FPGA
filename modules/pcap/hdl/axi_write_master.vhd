d4886f2a94fc3d386aec39daffc85f04a2575a2a
Emilio Perez Juarez <emiliopeju@gmail.com>
2025-12-01 14:37:57 +0000

    axi_write_master: improve code style

--- modules/pcap/hdl/axi_write_master.vhd
+++ modules/pcap/hdl/axi_write_master.vhd
@@ -22,7 +22,7 @@ port (
     clk_i               : in  std_logic;
     reset_i             : in  std_logic;
     -- AXI Transaction Parameters
-    M_AXI_BURST_LEN     : in  std_logic_vector(AXI_BURST_WIDTH-1 downto 0);
+    m_axi_burst_len     : in  std_logic_vector(AXI_BURST_WIDTH-1 downto 0);
     -- AXI HP Bus Write Only Interface
     m_axi_awready       : in  std_logic;
     m_axi_awregion      : out std_logic_vector(3 downto 0);
@@ -53,11 +53,11 @@ port (
     dma_done            : out std_logic;
     dma_error           : out std_logic
 );
-end axi_write_master;
+end;
 
 architecture rtl of axi_write_master is
 
-signal AXI_BURST_LEN        : unsigned(AXI_BURST_WIDTH-1 downto 0);
+signal axi_burst_len        : unsigned(AXI_BURST_WIDTH-1 downto 0);
 signal awvalid              : std_logic := '0';
 signal wvalid               : std_logic := '0';
 signal wlast                : std_logic;
@@ -72,43 +72,43 @@ begin
 --
 -- Write Address
 --
-M_AXI_AWREGION <= "0000";
+m_axi_awregion <= "0000";
 -- Single threaded
-M_AXI_AWID <= "000000";
+m_axi_awid <= "000000";
 -- Burst Length is number of transaction beats, minus 1
-AXI_BURST_LEN <= unsigned(M_AXI_BURST_LEN) - 1;
-M_AXI_AWLEN <= std_logic_vector(AXI_BURST_LEN(3 downto 0));
+axi_burst_len <= unsigned(m_axi_burst_len) - 1;
+m_axi_awlen <= std_logic_vector(AXI_BURST_LEN(3 downto 0));
 -- Size should be AXI_DATA_WIDTH, in 2^SIZE bytes
-M_AXI_AWSIZE <= TO_SVECTOR(LOG2(AXI_DATA_WIDTH/8), 3);
+m_axi_awsize <= TO_SVECTOR(LOG2(AXI_DATA_WIDTH/8), 3);
 -- INCR burst type is usually used, except for keyhole bursts
-M_AXI_AWBURST <= "01";
+m_axi_awburst <= "01";
 -- AXI3 atomic access encoding for Normal acces
-M_AXI_AWLOCK <= "00";
+m_axi_awlock <= "00";
 -- Cacheable write-through, no allocate
-M_AXI_AWCACHE <= "0010";
+m_axi_awcache <= "0010";
 -- Protection encoding for 'Non-secure access'
-M_AXI_AWPROT <= "000";
+m_axi_awprot <= "000";
 -- Not participating in any QoS scheme
-M_AXI_AWQOS <= "0000";
+m_axi_awqos <= "0000";
 
-M_AXI_AWVALID <= awvalid;
+m_axi_awvalid <= awvalid;
 
-M_AXI_AWADDR <= dma_addr;
+m_axi_awaddr <= dma_addr;
 
 --
 -- Write Data
 --
-M_AXI_WDATA <= dma_data;
+m_axi_wdata <= dma_data;
 
 -- All bursts are complete and aligned
-M_AXI_WSTRB <= (others => '1');
-M_AXI_WLAST <= wlast;
-M_AXI_WVALID <= wvalid;
+m_axi_wstrb <= (others => '1');
+m_axi_wlast <= wlast;
+m_axi_wvalid <= wvalid;
 
 --
 -- Write Response
 --
-M_AXI_BREADY <= bready;
+m_axi_bready <= bready;
 
 --
 -- The purpose of the write address channel is to request the address and
@@ -119,29 +119,27 @@ M_AXI_BREADY <= bready;
 -- completed.
 --
 
-WRITE_ADDR_CHANNEL: process(clk_i)
+write_addr_channel: process(clk_i)
 begin
     if rising_edge(clk_i) then
-        if (reset_i = '1') then
+        if reset_i then
             awvalid <= '0';
         -- If previously not valid and no throttling, start next transaction
-        elsif (awvalid = '0' and aw_throttle = '0') then
+        elsif not awvalid and not aw_throttle then
             awvalid <= '1';
         -- Once asserted, VALIDs cannot be deasserted, so AWVALID
         -- must wait until transaction is accepted before throttling.
-        elsif (M_AXI_AWREADY = '1' and awvalid = '1') then
+        elsif m_axi_awready and awvalid then
             awvalid <= '0';
-        else
-            awvalid <= awvalid;
         end if;
 
         -- aw_throttle is used to issue one address per dma start
         -- for synchronisation with data channel
-        if (reset_i = '1') then
+        if reset_i then
             aw_throttle <= '1';
-        elsif (dma_start = '1') then
+        elsif dma_start then
             aw_throttle <= '0';
-        elsif (awvalid = '1' and M_AXI_AWREADY = '1' and aw_throttle = '0') then
+        elsif awvalid and m_axi_awready and not aw_throttle then
             aw_throttle <= '1';
         else
             aw_throttle <= aw_throttle;
@@ -159,31 +157,29 @@ end process;
 --
 
 -- Forward movement occurs when the channel is valid and ready
-wnext <= M_AXI_WREADY and wvalid;
+wnext <= m_axi_wready and wvalid;
 
-WRITE_DATA_CHANNEL: process(clk_i)
+write_data_channel: process(clk_i)
 begin
     if rising_edge(clk_i) then
-        if (reset_i = '1') then
+        if reset_i then
             wvalid <= '0';
         -- If previously not valid and not throttling, start next transaction
-        elsif (wvalid = '0' and w_throttle = '0') then
+        elsif not wvalid and not w_throttle then
             wvalid <= '1';
         -- Once asserted, VALIDs cannot be deasserted, so WVALID
         -- must wait until burst is complete with WLAST
-        elsif (wnext = '1' and wlast = '1') then
+        elsif wnext and wlast then
             wvalid <= '0';
-        else
-            wvalid <= wvalid;
         end if;
 
         -- w_throttle is used to issue one data burst per dma start
         -- for synchronisation with addr channel
-        if (reset_i = '1') then
+        if reset_i then
             w_throttle <= '1';
-        elsif (dma_start = '1') then
+        elsif dma_start then
             w_throttle <= '0';
-        elsif (wvalid = '1' and M_AXI_WREADY = '1' and w_throttle = '0') then
+        elsif wvalid and m_axi_wready and not w_throttle then
             w_throttle <= '1';
         else
             w_throttle <= w_throttle;
@@ -192,19 +188,17 @@ begin
 end process;
 
 -- WLAST generation on the MSB of a counter underflow
-wlast <= '1' when (wlen_count = AXI_BURST_LEN and wnext = '1') else '0';
+wlast <= '1' when wlen_count = axi_burst_len and wnext = '1' else '0';
 
 -- Burst length counter. Uses extra counter register bit to indicate terminal
 -- count to reduce decode logic
-BURST_LENGTH_COUNT: process(clk_i)
+burst_length_count: process(clk_i)
 begin
     if rising_edge(clk_i) then
-        if (reset_i = '1' or wlast = '1') then
+        if reset_i or wlast then
             wlen_count <= (others => '0');
-        elsif (wnext = '1') then
+        elsif wnext then
             wlen_count <= wlen_count + 1;
-        else
-            wlen_count <= wlen_count;
         end if;
     end if;
 end process;
@@ -212,11 +206,11 @@ end process;
 -- Always accept a write response from slave
 bready <= '1';
 
-dma_done <= M_AXI_BVALID;
+dma_done <= m_axi_bvalid;
 
 -- The BRESP is used indicate any errors from the interconnect or
 -- slave for the entire write burst.
-dma_error <= M_AXI_BVALID and (M_AXI_BRESP(1) or M_AXI_BRESP(0));
+dma_error <= m_axi_bvalid and (m_axi_bresp(1) or m_axi_bresp(0));
 dma_read <= wnext;
 
-end rtl;
+end;
