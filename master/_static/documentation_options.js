d1a0621026164d11725ce4730cdcc191430f4945
tomtrafford <tomtrafford@users.noreply.github.com>
2025-12-04 11:29:41 +0000

    deploy: d1715cfb77f9afa9243d5cd58a0018f128e58b38

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1-14-gde178de',
+    VERSION: '4.1-15-gd1715cf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
